5806043a311231db76841f595e6f66b8a390075c
HoangLee <hoangleenhan@gmail.com>
2016-10-10 14:20:04 +0700

    fix defautl settings in course metabox

--- inc/libraries/meta-box/inc/fields/number.php
+++ inc/libraries/meta-box/inc/fields/number.php
@@ -1,44 +1,47 @@
 <?php
 // Prevent loading this file directly
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-if ( ! class_exists( 'RWMB_Number_Field' ) ) {
-	class RWMB_Number_Field extends RWMB_Field {
-		/**
-		 * Get field HTML
-		 *
-		 * @param mixed $meta
-		 * @param array $field
-		 *
-		 * @return string
-		 */
-		static function html( $meta, $field ) {
-			return sprintf(
-				'<input type="number" class="rwmb-number" name="%s" id="%s" value="%s" step="%s" min="%s" max="%s" placeholder="%s"/>',
-				$field['field_name'],
-				empty( $field['clone'] ) ? $field['id'] : '',
+if (!class_exists('RWMB_Number_Field')) {
+    class RWMB_Number_Field extends RWMB_Field
+    {
+        /**
+         * Get field HTML
+         *
+         * @param mixed $meta
+         * @param array $field
+         *
+         * @return string
+         */
+        static function html($meta, $field)
+        {
+            return sprintf(
+                '<input type="number" class="rwmb-number" name="%s" id="%s" value="%s" step="%s" min="%s" max="%s" placeholder="%s"/>',
+                $field['field_name'],
+                empty($field['clone']) ? $field['id'] : '',
                 $meta = isset($field['std']) ? $field['std'] : ($meta ? $meta : ''),
-				$field['step'],
-				$field['min'],
-                                ! empty( $field['max'] ) ? $field['max'] : '',
-				$field['placeholder']
-			);
-		}
+                $field['step'],
+                $field['min'],
+                !empty($field['max']) ? $field['max'] : '',
+                $field['placeholder']
+            );
+        }
 
-		/**
-		 * Normalize parameters for field
-		 *
-		 * @param array $field
-		 *
-		 * @return array
-		 */
-		static function normalize_field( $field ) {
-			$field = wp_parse_args( $field, array(
-				'step' => 1,
-				'min'  => 0,
-			) );
+        /**
+         * Normalize parameters for field
+         *
+         * @param array $field
+         *
+         * @return array
+         */
+        static function normalize_field($field)
+        {
+            $field = wp_parse_args($field, array(
+                'step' => 1,
+                'min' => 0,
+            ));
 
-			return $field;
-		}
-	}
+            return $field;
+        }
+    }
 }
