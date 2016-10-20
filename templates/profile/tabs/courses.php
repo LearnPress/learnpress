eb0bc6e163c21045e32203168842f68280a7f825
HoangLee <hoangleenhan@gmail.com>
2016-10-20 09:57:07 +0700

    Fix template version in profile page

--- templates/profile/tabs/courses.php
+++ templates/profile/tabs/courses.php
@@ -4,7 +4,7 @@
  *
  * @author  ThimPress
  * @package LearnPress/Templates
- * @version 1.0
+ * @version 2.0
  */
 
 if ( !defined( 'ABSPATH' ) ) {
--- templates/profile/tabs/courses/index.php
+++ templates/profile/tabs/courses/index.php
@@ -2,7 +2,7 @@
 /**
  * @author  ThimPress
  * @package LearnPress/Templates
- * @version 1.0
+ * @version 2.0
  */
 
 if ( !defined( 'ABSPATH' ) ) {
