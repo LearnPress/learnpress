11d6d46e57b98a2dd67c6a7071b353b9a40e0fe9
Phong Lo <phonglq@foobla.com>
2016-10-22 10:22:17 +0700

    Edit User Profile: add fields nickname, displayname

--- templates/profile/edit.php
+++ templates/profile/edit.php
@@ -13,8 +13,13 @@ global $wp_query;
 $user = learn_press_get_current_user();
 
 $user_info = get_userdata( $user->id );
+//var_dump($user_info);
+//$vars = get_object_vars($user_info);
+//echo '<pre>'.print_r($vars, true).'</pre>';
+//$profileuser = get_user_to_edit($user_id);
 
 $username             = $user_info->user_login;
+$nick_name				= $user_info->nickname;
 $first_name           = $user_info->first_name;
 $last_name            = $user_info->last_name;
 $profile_picture_type = get_user_meta( $user->id, '_lp_profile_picture_type', true );
@@ -80,6 +85,46 @@ if ( $user ) :
 			<div class="user-last-name-wrap info-field">
 				<p class="profile-field-name"><?php esc_html_e( 'Last Name', 'learnpress' ) ?></p>
 				<input type="text" name="last_name" id="last_name" value="<?php esc_attr_e( $last_name ); ?>" class="regular-text">
+			</div>
+			
+			<div class="user-nickname-wrap info-field">
+				<p class="profile-field-name"><?php _e('Nickname'); ?><span class="description"><?php _e('(required)'); ?></span></p>
+				<td><input type="text" name="nickname" id="nickname" value="<?php echo esc_attr($user_info->nickname) ?>" class="regular-text" /></td>
+			</div>
+			<div class="user-last-name-wrap info-field">
+				<p class="profile-field-name"><?php esc_html_e( 'Display name publicly as', 'learnpress' ) ?></p>
+				<select name="display_name" id="display_name">
+				<?php
+					$public_display = array();
+					$public_display['display_nickname']  = $user_info->nickname;
+					$public_display['display_username']  = $user_info->user_login;
+
+					if ( !empty($user_info->first_name) )
+						$public_display['display_firstname'] = $user_info->first_name;
+
+					if ( !empty($user_info->last_name) )
+						$public_display['display_lastname'] = $user_info->last_name;
+
+					if ( !empty($user_info->first_name) && !empty($user_info->last_name) ) {
+						$public_display['display_firstlast'] = $user_info->first_name . ' ' . $user_info->last_name;
+						$public_display['display_lastfirst'] = $user_info->last_name . ' ' . $user_info->first_name;
+					}
+
+					if ( !in_array( $user_info->display_name, $public_display ) ) // Only add this if it isn't duplicated elsewhere
+					{
+						$public_display = array( 'display_displayname' => $user_info->display_name ) + $public_display;
+					}
+
+					$public_display = array_map( 'trim', $public_display );
+					$public_display = array_unique( $public_display );
+
+					foreach ( $public_display as $id => $item ) {
+				?>
+						<option <?php selected( $user_info->display_name, $item ); ?>><?php echo $item; ?></option>
+				<?php
+					}
+				?>
+				</select>
 			</div>
 
 			<h2><?php _e( 'Contact Info', 'learnpress' ); ?></h2>
