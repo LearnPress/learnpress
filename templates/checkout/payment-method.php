7b990d83ebe1483409db800ff1250e12fd9432af
HoangLee <hoangleenhan@gmail.com>
2016-10-24 11:32:03 +0700

    Fix checkout payment method

--- templates/checkout/payment-method.php
+++ templates/checkout/payment-method.php
@@ -10,7 +10,6 @@
 if ( !defined( 'ABSPATH' ) ) {
 	exit;
 }
-
 ?>
 
 <?php if ( $gateway instanceof LP_Gateway_Abstract ) : ?>
@@ -18,7 +17,7 @@ if ( !defined( 'ABSPATH' ) ) {
 	<?php if ( apply_filters( 'learn_press_display_payment_method', true, $gateway->id ) ) { ?>
 		<li>
 			<label>
-				<input id="payment_method_<?php echo $gateway->id; ?>" type="radio" class="input-radio" <?php checked($selected, true);?> name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( LP()->session->get( 'chosen_payment_method' ) == $gateway->id, true ); ?> data-order_button_text="<?php echo esc_attr( $gateway->order_button_text ); ?>" />
+				<input id="payment_method_<?php echo $gateway->id; ?>" type="radio" class="input-radio" <?php checked( $selected, $gateway->id, true ); ?> name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( LP()->session->get( 'chosen_payment_method' ) == $gateway->id, true ); ?> data-order_button_text="<?php echo esc_attr( $gateway->order_button_text ); ?>" />
 				<?php echo( $gateway->get_title() ); ?>
 			</label>
 			<?php if ( ( $payment_form = $gateway->get_payment_form() ) || ( $payment_form = $gateway->get_description() ) ) { ?>
