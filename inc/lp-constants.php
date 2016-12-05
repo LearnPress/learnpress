63f790738935680674666a2366aeb1654dfa91af
Tu <tunn@foobla.com>
2016-12-05 10:10:10 +0700

    + Improved check outdated templates + Removed automating create "LP Cart" page while installing

--- inc/lp-constants.php
+++ inc/lp-constants.php
@@ -4,7 +4,7 @@
  */
 $upload_dir = wp_upload_dir();
 // version
-define( 'LEARNPRESS_VERSION', '2.0.6' );
+define( 'LEARNPRESS_VERSION', '2.0.6.1' );
 
 define( 'LP_WP_CONTENT', basename( WP_CONTENT_DIR ) );
 
