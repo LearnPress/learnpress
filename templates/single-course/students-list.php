83759628c0730e9927888e2b40e95c7647483460
haihld_58 <hoangleenhan@gmail.com>
2016-12-14 17:11:55 +0700

    Show progress bar of students list enrolled course

--- templates/single-course/students-list.php
+++ templates/single-course/students-list.php
@@ -25,37 +25,48 @@ $students_list_avatar_size = apply_filters( 'learn_press_students_list_avatar_si
 	<?php endif; ?>
 
 	<?php if ( $students = $course->get_students_list( true, $student_limit ) ): ?>
+
+		<?php $passing_condition = round( $course->passing_condition, 0 ); ?>
+
         <ul class="students">
 			<?php foreach ( $students as $student ):
+				$student = LP_User_Factory::get_user( $student->ID );
+				$result = $student->get_course_info2( $course->ID );
 				?>
+
                 <li>
-					<?php if ( $show_avatar ): ?>
-						<?php echo get_avatar(
-							$student->ID,
-							$students_list_avatar_size,
-							'',
-							$student->display_name,
-							array(
-								'class' => 'students_list_avatar'
-							)
-						); ?>
-					<?php endif; ?>
-                    <a
-                        class="name"
-                        href="<?php echo learn_press_user_profile_link( $student->ID ) ?>"
-                        title="<?php echo $student->display_name . ' profile' ?>"
-                    >
-						<?php echo $student->display_name ?>
-                    </a>
+                    <div class="user-info">
+						<?php if ( $show_avatar ): ?>
+							<?php echo get_avatar( $student->ID, $students_list_avatar_size, '', $student->display_name, array( 'class' => 'students_list_avatar' ) ); ?>
+						<?php endif; ?>
+                        <a class="name" href="<?php echo learn_press_user_profile_link( $student->ID ) ?>" title="<?php echo $student->display_name . ' profile' ?>">
+							<?php echo $student->display_name ?>
+                        </a>
+                    </div>
+                    <div class="learn-press-course-results-progress">
+                        <div class="course-progress">
+                            <span class="course-result"><?php echo $result['results'] . '%'; ?></span>
+                            <div class="lp-course-progress">
+                                <div class="lp-progress-bar">
+                                    <div class="lp-progress-value" style="width: <?php echo $result['results']; ?>%;">
+                                    </div>
+                                </div>
+                                <div class="lp-passing-conditional"
+                                     data-content="<?php printf( esc_html__( 'Passing condition: %s%%', 'learnpress' ), $passing_condition ); ?>"
+                                     style="left: <?php echo esc_attr( $passing_condition ); ?>%;">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </li>
 			<?php endforeach; ?>
-			<?php
-			$other_student = $course->students;
-			if ( $other_student ) {
-				echo '<p>and ' . sprintf( _n( 'one student enrolled.', '%s students enrolled.', $other_student, 'learnpress' ), $other_student ) . '</p>';
-			}
-			?>
         </ul>
+		<?php
+		$other_student = $course->students;
+		if ( $other_student ) {
+			echo '<p>and ' . sprintf( _n( 'one student enrolled.', '%s students enrolled.', $other_student, 'learnpress' ), $other_student ) . '</p>';
+		}
+		?>
 	<?php else: ?>
         <div class="students empty">
 			<?php if ( $course->students ) {
