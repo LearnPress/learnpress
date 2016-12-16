e128149f939151308e7a4239be57af0a69199323
Chinhtm <chinhtm@foobla.com>
2016-12-16 16:02:46 +0700

    Premium add-ons

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -82,7 +82,7 @@
 
 	function _ready() {
 		LP_Admin.init();
-		$(document).on('click', '.learn-press-add-ons .plugin-action-buttons a', function (e) {
+		$(document).on('click', '.learn-press-add-ons .plugin-action-buttons a:not(.lp-not-ajax)', function (e) {
 			e.preventDefault();
 			var $plugin = $(this).closest('.plugin-card');
 			if ($(this).hasClass('button-working')) {
@@ -466,7 +466,7 @@ lprHook.addAction('lpr_admin_quiz_question_html', _lprAdminQuestionHTML);
 				})
 			})
 			.lprFancyCheckbox();
-		$('#learn-press-add-ons-wrap').on('click', '.plugin-action-buttons a', function (evt) {
+		$('#learn-press-add-ons-wrap').on('click', '.plugin-action-buttons a:not(.lp-not-ajax)', function (evt) {
 			evt.preventDefault();
 			var $link = $(this), action = $link.data('action');
 			if (!action) return;
