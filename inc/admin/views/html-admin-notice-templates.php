11f5a894bbf6733f0a959a2472f18a8ace89bfd7
Tu <tunn@foobla.com>
2017-02-07 07:36:52 +0700

    prerelease 2.1.3

--- inc/admin/views/html-admin-notice-templates.php
+++ inc/admin/views/html-admin-notice-templates.php
@@ -1,7 +1,11 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
+if ( !defined( 'ABSPATH' ) ) {
 	exit;
+}
+
+if ( learn_press_get_user_option( 'hide-notice-template-files' ) == 'yes' ) {
+	return;
 }
 $template_dir   = get_template_directory();
 $stylesheet_dir = get_stylesheet_directory();
@@ -31,4 +35,5 @@ $theme_name = implode( ' & ', $theme_name );
 	<p>
 		<a class="button" href="<?php echo esc_url( admin_url( 'admin.php?page=learn-press-tools&tab=templates' ) ); ?>"><?php _e( 'View list of outdated templates', 'learnpress' ); ?></a>
 	</p>
+	<a href="<?php echo esc_url( add_query_arg( 'lp-hide-notice', 'template-files', learn_press_get_current_url() ) ); ?>" class="learn-press-admin-notice-dismiss"></a>
 </div>
