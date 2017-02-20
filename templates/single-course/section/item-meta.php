21a06e34797f8262341e017d300e1140897184cc
Tu <tunn@foobla.com>
2017-02-20 11:03:14 +0700

    Removed "Preview" label for no require enroll course

--- templates/single-course/section/item-meta.php
+++ templates/single-course/section/item-meta.php
@@ -2,7 +2,7 @@
 /**
  * @author        ThimPress
  * @package       LearnPress/Templates
- * @version       1.0
+ * @version       2.1.5
  */
 
 defined( 'ABSPATH' ) || exit();
@@ -12,22 +12,19 @@ $course      = LP()->global['course'];
 $item_status = $user->get_item_status( $item->ID );
 $security    = wp_create_nonce( sprintf( 'complete-item-%d-%d-%d', $user->id, $course->id, $item->ID ) );
 $course_item = $course->get_item( $item->ID );
-$status = $user->get_course_status( $course->id );
+$status      = $user->get_course_status( $course->id );
 ?>
 <div class="course-item-meta">
-
 	<?php do_action( 'learn_press_before_item_meta', $item ); ?>
-
-	<!--<span class="lp-label lp-label-viewing"><?php _e( 'Viewing', 'learnpress' ); ?></span>-->
-	<?php if ( $status != 'enrolled' && $course_item->is_preview() ): ?>
+	<?php if ( $status != 'enrolled' && $course_item->is_preview() && $course->is_required_enroll() ): ?>
 		<span class="lp-label lp-label-preview"><?php _e( 'Preview', 'learnpress' ); ?></span>
 	<?php endif; ?>
 	<?php if ( $user->can_view_item( $item->ID, $course->id ) !== false ) { ?>
 		<?php if ( $item->post_type == 'lp_quiz' ) { ?>
 			<span class="item-loop-meta-text item-result">
-				<?php if( $result = $user->get_quiz_results( $item->ID )):?>
-				<?php echo sprintf( '%d%%', round( $result->mark_percent ) ); ?>
-				<?php endif;?>
+				<?php if ( $result = $user->get_quiz_results( $item->ID ) ): ?>
+					<?php echo sprintf( '%d%%', round( $result->mark_percent ) ); ?>
+				<?php endif; ?>
 			</span>
 		<?php } ?>
 		<?php if ( $item_status == 'completed' ) { ?>
@@ -38,9 +35,5 @@ $status = $user->get_course_status( $course->id );
 			<span class="lp-icon item-status button-complete-item button-complete-lesson" data-security="<?php echo esc_attr( $security ); ?>" title="<?php esc_attr_e( 'Not Started', 'learnpress' ); ?>"></span>
 		<?php } ?>
 	<?php } ?>
-
-	<?php //learn_press_item_meta_type( $course, $item ); ?>
-
 	<?php do_action( 'learn_press_after_item_meta', $item ); ?>
-
 </div>
