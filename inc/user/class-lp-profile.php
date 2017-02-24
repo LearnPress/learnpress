dfb83511945edb2573fc4dbf2b75011b6500a76b
Tu <tunn@foobla.com>
2017-02-24 16:54:08 +0700

    Profile

--- inc/user/class-lp-profile.php
+++ inc/user/class-lp-profile.php
@@ -6,15 +6,36 @@ if ( !defined( 'ABSPATH' ) ) {
 if ( !class_exists( 'LP_Profile' ) ) {
 	/**
 	 * Class LP_Profile
+	 *
+	 * Main class to controls the profile of a user
 	 */
 	class LP_Profile {
+		/**
+		 * The instances of all users has initialed a profile
+		 *
+		 * @var array
+		 */
+		protected static $_instances = array();
+
 		/**
 		 *  Constructor
 		 */
 		public function __construct() {
 
 		}
-	}
 
-	new LP_Profile;
+		/**
+		 * Get an instance of LP_Profile for a user id
+		 *
+		 * @param $user_id
+		 *
+		 * @return mixed
+		 */
+		public static function instance( $user_id ) {
+			if ( empty( self::$_instances[$user_id] ) ) {
+				self::$_instances[$user_id] = new self( $user_id );
+			}
+			return self::$_instances[$user_id];
+		}
+	}
 }
