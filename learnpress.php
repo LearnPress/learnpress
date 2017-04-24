b341233b1a3d6d4f1b8635a9f6aa571e1adf5774
Tu <tunn@foobla.com>
2017-04-24 15:50:47 +0700

    Fixed issue with wc addon

--- learnpress.php
+++ learnpress.php
@@ -297,7 +297,7 @@ if ( !class_exists( 'LearnPress' ) ) {
 			}
 
 			$this->get_user();
-			$this->gateways = LP_Gateways::instance()->get_available_payment_gateways();
+			$this->gateways = LP_Gateways::instance()->get_gateways();//$this->gateways = LP_Gateways::instance()->get_available_payment_gateways();
 			$this->schedule = require_once( LP_PLUGIN_PATH . "/inc/class-lp-schedules.php" );
 
 			LP_Emails::instance();
