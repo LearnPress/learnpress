52218781542005140b9d213529b63cbd1a1cf59d
tunnhn <tunnhn@gmail.com>
2017-05-15 10:12:15 +0700

    Remove widget fitlers

--- inc/widgets/popular-courses/popular-courses.php
+++ inc/widgets/popular-courses/popular-courses.php
@@ -85,7 +85,7 @@ if ( ! class_exists( 'LP_Widget_Popular_Courses' ) ) {
 			);
 
 			parent::__construct();
-			add_filter( 'learn_press_widget_display_content-' . $this->id_base, 'learn_press_is_course' );
+			//add_filter( 'learn_press_widget_display_content-' . $this->id_base, 'learn_press_is_course' );
 		}
 
 		/**
