51e6b8d17d4ab527e43218220c6e277919cc5b3b
lee <hai@foobla.com>
2017-06-07 09:38:37 +0700

    Add option to block lesson content if course is expired

---
+++ templates/content-lesson/block-content.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author  ThimPress
+ * @package LearnPress/Templates
+ * @version 2.1.7
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+learn_press_display_message( __( 'This lesson has been locked', 'learnpress' ), 'error' );
