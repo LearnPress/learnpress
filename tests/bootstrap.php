601ae097f17d4079fbb6413e5ef91b8624fa3ab6
Max <tutv95@gmail.com>
2017-07-13 15:43:37 +0700

    Updated config unit test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,19 +1,21 @@
 <?php
 /**
- * Bootstrap the testing environment
- * Uses wordpress tests (http://github.com/nb/wordpress-tests/) which uses PHPUnit
- * @package wordpress-plugin-tests
+ * PHPUnit bootstrap file
  *
- * Usage: change the below array to any plugin(s) you want activated during the tests
- *        value should be the path to the plugin relative to /wp-content/
- *
- * Note: Do note change the name of this file. PHPUnit will automatically fire this file when run.
- *
+ * @package Sample_Plugin
  */
-
-require dirname( __FILE__ ) . '../vendor/autoload.php';
-
-$GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( 'your-plugin/your-plugin.php' ),
-);
-require dirname( __FILE__ ) . '/lib/bootstrap.php';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( dirname( __FILE__ ) ) . '/learnpress.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
---
+++ tests/test-sample.php
@@ -0,0 +1,19 @@
+
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Sample_Plugin
+ */
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
