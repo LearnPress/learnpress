0b43506bfabd80e3d9f5fcb05f3d666c28273a48
Max <tutv95@gmail.com>
2017-07-26 10:57:52 +0700

    Updated interface CURD

--- inc/interfaces/interface-curd.php
+++ inc/interfaces/interface-curd.php
@@ -3,18 +3,33 @@
 /**
  * Interface LP_Interface_CURD
  */
-interface LP_Interface_CURD{
+interface LP_Interface_CURD {
+	/**
+	 * Create item and insert to database.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @param $args
+	 *
+	 * @return mixed
+	 */
+	public function create( $args );
+
 	/**
 	 * Load data from database.
+	 *
+	 * @since 3.0.0
 	 *
 	 * @param object $object
 	 *
 	 * @return mixed
 	 */
-	public function load(&$object);
+	public function load( &$object );
 
 	/**
 	 * Update data into database.
+	 *
+	 * @since 3.0.0
 	 *
 	 * @return mixed
 	 */
@@ -22,6 +37,8 @@ interface LP_Interface_CURD{
 
 	/**
 	 * Delete data from database.
+	 *
+	 * @since 3.0.0
 	 *
 	 * @return mixed
 	 */
