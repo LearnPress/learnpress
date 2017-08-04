826b492cb5f4425f42d664a70b250f33b4e11a0b
tunnhn <tunnhn@gmail.com>
2017-08-04 17:39:18 +0700

    Profile

--- templates/profile/profile-cover.php
+++ templates/profile/profile-cover.php
@@ -12,12 +12,15 @@ defined( 'ABSPATH' ) or exit;
 global $profile;
 
 $user = $profile->get_user();
-
+learn_press_get_course();
 ?>
 <div id="learn-press-profile-header" class="lp-profile-header">
     <div class="lp-profile-cover">
         <div class="lp-profile-avatar">
 			<?php echo $user->get_profile_picture(); ?>
+            <span class="profile-name"><?php echo $user->get_data( 'username' ); ?></span>
         </div>
     </div>
 </div>
+
+
