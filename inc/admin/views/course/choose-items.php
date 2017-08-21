4d7b982819f94fd526f5bacb9c330f5f08cf1aed
max <tutv95@gmail.com>
2017-08-21 16:46:05 +0700

    Updated choose items

--- inc/admin/views/course/choose-items.php
+++ inc/admin/views/course/choose-items.php
@@ -9,10 +9,32 @@ learn_press_admin_view( 'course/added-items-preview' );
 
 ?>
 
+<script type="text/x-template" id="tmpl-lp-course-choose-item">
+    <li class="section-item" :class="item.type" @click="$emit('add', item)">
+        <span class="icon"></span>
+        <span class="title">{{item.title}}</span>
+    </li>
+</script>
+
+<script>
+    (function () {
+
+        Vue.component('lp-course-choose-item', {
+            template: '#tmpl-lp-course-choose-item',
+            props: ['item']
+        });
+
+    })(Vue, LP_Curriculum_Store);
+</script>
+
 <script type="text/x-template" id="tmpl-lp-course-choose-items">
     <div id="lp-modal-choose-items" :class="{show:show}">
-        <div class="lp-choose-items">
+        <div class="lp-choose-items" :class="{'show-preview': showPreview}">
             <div class="header">
+                <div class="preview-title">
+                    <span>Selected items ({{addedItems.length}})</span>
+                </div>
+
                 <ul class="tabs">
                     <template v-for="(type, key) in types">
                         <li :data-type="key"
@@ -42,8 +64,7 @@ learn_press_admin_view( 'course/added-items-preview' );
                     </template>
 
                     <template v-else v-for="item in items">
-                        <li @click="addItem(item)"><span class="dashicons dashicons-plus"></span><span
-                                    v-html="item.title"></span></li>
+                        <lp-course-choose-item @add="addItem(item)" :item="item"></lp-course-choose-item>
                     </template>
                 </ul>
 
@@ -60,18 +81,18 @@ learn_press_admin_view( 'course/added-items-preview' );
 
             <div class="footer">
                 <div class="cart">
-                    <button type="button"
-                            @click.prevent="showPreview = !showPreview"
-                            class="button button-secondary edit-selected">
-                        {{textButtonEdit}}
-                    </button>
-
                     <button
                             @click="checkout"
                             :disabled="!addedItems.length"
                             type="button"
                             class="button button-primary checkout">
                         <span>Add ({{addedItems.length}})</span>
+                    </button>
+
+                    <button type="button"
+                            @click.prevent="showPreview = !showPreview"
+                            class="button button-secondary edit-selected">
+                        {{textButtonEdit}}
                     </button>
                 </div>
             </div>
