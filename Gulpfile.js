03bfdd2f7b93f0cb97f722155635519a88c28b32
tunnhn <tunnhn@gmail.com>
2017-11-13 10:41:52 +0700

    Added gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,9 @@
  * 1/ Run "npm install gulp -g" if you did not run it any time in the past.
  * 2/ Run "npm install gulp --save-dev" to install gulp in your project directory.
  * 3/ Run "npm install package-name[ package-name...] --save-dev
+ *
+ * EX: npm install gulp-zip gulp-copy gulp-clean gulp-sass gulp-livereload gulp-sourcemaps read-file gulp-replace mkdirp gulp-concat gulp-uglify pump --save-dev
+
  */
 'use strict';
 const zip = require('gulp-zip');
