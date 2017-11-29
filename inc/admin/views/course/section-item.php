30e05fda1ce198a6ac7286d4f3d76a8fa8a467e1
lee <hai@foobla.com>
2017-11-29 10:32:20 +0700

    Improve admin create course item

--- inc/admin/views/course/section-item.php
+++ inc/admin/views/course/section-item.php
@@ -5,9 +5,10 @@
  * @since 3.0.0
  */
 ?>
+<!--<div class="drag"><svg class="Icon DragIcon DragHandle-icon" viewBox="0 0 32 32"><path d="M 14 5.5 a 3 3 0 1 1 -3 -3 A 3 3 0 0 1 14 5.5 Z m 7 3 a 3 3 0 1 0 -3 -3 A 3 3 0 0 0 21 8.5 Z m -10 4 a 3 3 0 1 0 3 3 A 3 3 0 0 0 11 12.5 Z m 10 0 a 3 3 0 1 0 3 3 A 3 3 0 0 0 21 12.5 Z m -10 10 a 3 3 0 1 0 3 3 A 3 3 0 0 0 11 22.5 Z m 10 0 a 3 3 0 1 0 3 3 A 3 3 0 0 0 21 22.5 Z"></path></svg></div>-->
+
 <script type="text/x-template" id="tmpl-lp-section-item">
     <li :class="['section-item',item.type, {updating: updating, removing: removing}]" :data-item-id="item.id">
-
         <div class="icon"></div>
         <div class="title">
             <input v-model="item.title" type="text" title="title no-submit"
