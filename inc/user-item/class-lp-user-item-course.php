1409e589a85c60e20800793e8b59cbdda958cae0
lee <hoangleenhan@gmail.com>
2018-03-13 09:25:11 +0700

    Update get course grade

--- inc/user-item/class-lp-user-item-course.php
+++ inc/user-item/class-lp-user-item-course.php
@@ -238,7 +238,7 @@ class LP_User_Item_Course extends LP_User_Item implements ArrayAccess {
 				)
 			);
 
-			$results['grade'] = $this->is_finished() ? $this->_is_passed( $results['result'] ) : '';
+			$results['grade'] = $this->is_finished() ? $this->_is_passed( $results['result'] ) : 'in-progress';
 		}
 
 		if ( $prop === 'status' ) {
@@ -454,9 +454,9 @@ class LP_User_Item_Course extends LP_User_Item implements ArrayAccess {
 	/**
 	 * Get completed items.
 	 *
-	 * @param string $type       - Optional. Filter by type (such lp_quiz, lp_lesson) if passed
-	 * @param bool   $with_total - Optional. Include total if TRUE
-	 * @param int    $section_id - Optional. Get in specific section
+	 * @param string $type - Optional. Filter by type (such lp_quiz, lp_lesson) if passed
+	 * @param bool $with_total - Optional. Include total if TRUE
+	 * @param int $section_id - Optional. Get in specific section
 	 *
 	 * @return array|bool|mixed
 	 */
@@ -510,8 +510,8 @@ class LP_User_Item_Course extends LP_User_Item implements ArrayAccess {
 	/**
 	 * Get items completed by percentage.
 	 *
-	 * @param string $type       - Optional. Filter by type or not
-	 * @param int    $section_id - Optional. Get in specific section
+	 * @param string $type - Optional. Filter by type or not
+	 * @param int $section_id - Optional. Get in specific section
 	 *
 	 * @return float|int
 	 */
