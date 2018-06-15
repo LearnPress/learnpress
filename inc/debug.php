073c2233564f2288bed559eee4d42a16ea8e59fb
tunnhn <tunnhn@gmail.com>
2018-06-15 18:10:00 +0700

    debug

--- inc/debug.php
+++ inc/debug.php
@@ -6,7 +6,6 @@
 /**
  * use http://example.com?debug=yes to execute the code in this file
  */
-
 class LP_Unit_Test {
 	public static function init() {
 		add_action( 'get_header', array( __CLASS__, 'test_emails' ) );
@@ -14,12 +13,14 @@ class LP_Unit_Test {
 
 	public static function test_emails() {
 		global $wp_rewrite;
-		$emailer = LP_Emails::instance();
-		$email   = $emailer->emails['LP_Email_Completed_Order_User'];
-		$email->enable = true;
-		$email->trigger( 2147 );
-learn_press_debug($email);
-		die();
+		if ( get_post_type( 2147 ) === LP_ORDER_CPT ) {
+			$emailer       = LP_Emails::instance();
+			$email         = $emailer->emails['LP_Email_Completed_Order_User'];
+			$email->enable = true;
+			$email->trigger();
+			learn_press_debug( $email );
+			die();
+		}
 	}
 }
 
