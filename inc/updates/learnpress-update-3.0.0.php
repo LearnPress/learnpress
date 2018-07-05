53ca726033335ba459e87ef2ed22b61b719fa3c2
Phong Lo <phonglq@foobla.com>
2018-07-05 11:06:48 +0700

    add more logs for upgrade database from 2.1 to 3.0

--- inc/updates/learnpress-update-3.0.0.php
+++ inc/updates/learnpress-update-3.0.0.php
@@ -45,8 +45,9 @@ class LP_Update_30 extends LP_Update_Base {
 			ADD COLUMN `start_time_gmt` DATETIME NULL DEFAULT %s AFTER `start_time`,
 			ADD COLUMN `end_time_gmt` DATETIME NULL DEFAULT %s AFTER `end_time`;
 		", '0000-00-00 00:00:00', '0000-00-00 00:00:00' );
-		@$wpdb->query( $sql );
-
+		$wpdb->query( $sql );
+		echo $sql;
+		echo "\n".$wpdb->last_error."\n";
 		// Update start_time_gmt, end_time_gmt with offset time from start_time, end_time
 		$time      = new LP_Datetime();
 		$offset    = $time->getOffset( true );
@@ -58,21 +59,24 @@ class LP_Update_30 extends LP_Update_Base {
 //			where start_time <> '0000:00:00 00:00:00'
 //			      AND start_time_gmt = '' OR start_time_gmt = '0000:00:00 00:00:00'
 //			LIMIT 0, 100");
-
+// exit('' . __LINE__ );
 		echo $sql = $wpdb->prepare( "
 			UPDATE {$wpdb->learnpress_user_items}
 			SET 
 				start_time_gmt = IF(start_time = %s, %s, DATE_ADD(start_time, INTERVAL %f HOUR)),
 				end_time_gmt = IF(end_time = %s, %s, DATE_ADD(end_time, INTERVAL %f HOUR))
 		", $null_time, $null_time, $offset, $null_time, $null_time, $offset );
-		@$wpdb->query( $sql );
-
+		$wpdb->query( $sql );
+		echo $sql;
+		echo "\n".$wpdb->last_error."\n";
 		echo $sql = $wpdb->prepare( "
 			ALTER TABLE {$wpdb->learnpress_user_items}
 			CHANGE COLUMN `user_id` `user_id` BIGINT(20) NOT NULL DEFAULT %d ,
 			CHANGE COLUMN `item_id` `item_id` BIGINT(20) NOT NULL DEFAULT %d ;
 		", - 1, - 1 );
-		@$wpdb->query( $sql );
+		$wpdb->query( $sql );
+		echo $sql;
+		echo "\n".$wpdb->last_error."\n";
 		$log = ob_get_clean();
 		LP_Debug::rollbackTransaction();
 
