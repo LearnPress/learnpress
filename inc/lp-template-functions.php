05799421bc3513892b3a50fad8874c16a8adf98a
Phong Lo <phonglq@foobla.com>
2018-08-10 11:01:32 +0700

    Fixed bug: answer correct all question but quiz result is failse with 0%

--- inc/lp-template-functions.php
+++ inc/lp-template-functions.php
@@ -3773,7 +3773,7 @@ function learn_press_get_link_current_question_instead_of_continue_button( $link
 		$user      = LP_Global::user();
 		$course    = $item->get_course();
 		$quiz_data = $user->get_item_data( $item->get_id(), $course->get_id() );
-		if ( $quiz_data->get_status() === 'started' ) {
+		if ( $quiz_data && $quiz_data->get_status() === 'started' ) {
 			$link = $item->get_question_link( $quiz_data->get_current_question() );
 		}
 	}
