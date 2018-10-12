40935e8f487415abcbc3312b2c2b867ef1e92fa4
tunnhn <tunnhn@gmail.com>
2018-10-12 09:09:59 +0700

    idn

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,38 +1,67 @@
-// npm i -g gulp
+/**
+ * Gulp tasks
+ *
+ * 1/ Run "npm install gulp -g" if you did not run it any time in the past.
+ * 2/ Run "npm install gulp --save-dev" to install gulp in your project directory.
+ * 3/ Run "npm install package-name[ package-name...] --save-dev
+ *
+ * EX: npm install gulp gulp-zip gulp-copy gulp-clean gulp-sass gulp-livereload gulp-sourcemaps read-file gulp-replace mkdirp gulp-concat gulp-uglify gulp-clean-css pump --save-dev
+
+ */
 'use strict';
 const zip = require('gulp-zip');
 
 var gulp = require('gulp'),
     gulpCopy = require('gulp-copy'),
     clean = require('gulp-clean'),
-    less = require('gulp-less'),
+    scss = require('gulp-sass'),
     liveReload = require('gulp-livereload'),
     sourceMaps = require('gulp-sourcemaps'),
     readFile = require('read-file'),
     replace = require('gulp-replace'),
-    mkdirp = require("mkdirp");
+    mkdirp = require("mkdirp"),
+    concat = require('gulp-concat'),
+    cleanCSS = require('gulp-clean-css');
 
-gulp.task('less', function () {
-    return gulp.src(['assets/less/**/*.less'])
+gulp.task('scss', function () {
+    return gulp.src(['assets/scss/**/*.scss'])
         .pipe(sourceMaps.init())
-        .pipe(less())
-        .pipe(sourceMaps.write())
+        .pipe(scss())
+        //.pipe(sourceMaps.write())
         .pipe(gulp.dest('assets/css'))
-        .pipe(liveReload());
+    //.pipe(liveReload());
 });
 
 gulp.task('watch', function () {
     liveReload.listen();
-    gulp.watch(['assets/less/**/*.less'], ['less']);
+    gulp.watch(['assets/scss/**/*.scss'], ['scss']);
+    gulp.watch(['assets/js/admin/utils/*.js'], ['compress-js']);
 });
 
-gulp.task('default', ['less', 'watch']);
+gulp.task('default', ['scss', 'watch', 'compress-js']);
+
+
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+
+gulp.task('compress-js', function (cb) {
+    return gulp.src('assets/js/admin/utils/*.js')
+        .pipe(concat('utils.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/js/admin'))
+});
+
+gulp.task('compress-css', function () {
+    return gulp.src('./assets/**/*.css')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./assets'));
+});
 
 /*
  * SVN: Copy working directory to SVN and prepare something before submitting.
  */
 var rootPath = '/Users/tu/Documents/foobla',
-    svnPath = rootPath + '/wporg/learnpress',
+    svnPath = rootPath + '/svn/learnpress',
     releasePath = rootPath + '/releases/learnpress',
     svnTrunkPath = svnPath + '/trunk',
     svnTagsPath = svnPath + '/tags',
@@ -56,6 +85,14 @@ var rootPath = '/Users/tu/Documents/foobla',
     updateReadme = function (version, callback) {
         return gulp.src(['readme.txt'])
             .pipe(replace(/Stable tag: (.*)/g, 'Stable tag: ' + version))
+            .pipe(gulp.dest(svnTrunkPath, {overwrite: true}))
+            .on('end', function () {
+                callback ? callback() : 'do nothing';
+            });
+    },
+    removeConst = function (callback) {
+        return gulp.src(['inc/lp-constants.php'])
+            .pipe(replace(/define\( 'LP_DEBUG_DEV'(.*)/g, ''))
             .pipe(gulp.dest(svnTrunkPath, {overwrite: true}))
             .on('end', function () {
                 callback ? callback() : 'do nothing';
@@ -106,7 +143,7 @@ gulp.task('release', ['copy-release'], function () {
 });
 
 // main task
-gulp.task('svn', ['less', 'copy-trunk'], function () {
+gulp.task('svn', ['scss', 'copy-trunk'], function () {
     updateReadme(getCurrentVer(true), function () {
         return gulp.start('release', ['copy-tag']);
     })
@@ -119,7 +156,6 @@ gulp.task('clr-zip', function () {
 
 gulp.task('copy-zip', ['clr-zip'], function () {
     mkdirp(releasePath);
-    //process.chdir(svnTrunkPath);
     var copyFiles = copySvnFiles;
     copyFiles.push('readme.txt');
     return gulp.src(copyFiles).pipe(gulpCopy(releasePath));
@@ -135,6 +171,61 @@ gulp.task('mk-zip', ['copy-zip'], function () {
 
 gulp.task('zip', ['mk-zip'], function () {
 
+});
+
+gulp.task('scss-popup', function () {
+    return gulp.src(['assets/scss/frontend/_item-popup.scss'])
+        .pipe(scss())
+        .pipe(gulp.dest('assets/css/frontend'))
+});
+
+/**
+ * Join and compress front-end script
+ */
+gulp.task('cfjs', function () {
+    return gulp.src([
+        'assets/js/vendor/watch.js',
+        'assets/js/vendor/jquery.alert.js',
+        'assets/js/vendor/jquery-scrollbar/jquery.scrollbar.js',
+        'assets/js/vendor/jquery.scrollTo.js',
+        'assets/js/learnpress.js',
+        'assets/js/frontend/course.js',
+        'assets/js/frontend/quiz.js',
+        //'assets/js/frontend/profile.js',
+        //'assets/js/frontend/become-teacher.js',
+    ])
+        .pipe(concat('learnpress-frontend.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/js/frontend'))
+});
+
+/**
+ * Join and compress frontend css
+ */
+gulp.task('cfcss', function () {
+    return gulp.src([
+        'assets/css/font-awesome.min.css',
+        'assets/js/vendor/jquery-scrollbar/jquery.scrollbar.css',
+        'assets/css/learnpress.css',
+    ])
+        .pipe(concat('learnpress-frontend.min.css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('assets/css'))
+});
+
+gulp.task('mk-zip', ['copy-zip'], function () {
+    process.chdir(releasePath);
+    var zipPath = releasePath.replace(/learnpress/, '');
+    return gulp.src(zipPath + '/**/learnpress/**/*')
+        .pipe(zip('learnpress.' + getCurrentVer(true) + '.zip'))
+        .pipe(gulp.dest(zipPath));
+});
+
+gulp.task('zipx', ['mk-zip'], function () {
+    var zipPath = releasePath + '.' + currentVer + '.zip';
+    console.log(zipPath)
+    return gulp.src([zipPath])
+        .pipe(gulpCopy("/Users/tu/Documents/htdocs/"));
 })
 
 // end of the world!
