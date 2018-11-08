6a4f172c2168e083c2fefd72303e0e68a8de3fc6
tunnhn <tunnhn@gmail.com>
2018-11-08 22:31:55 +0700

    Added new files

---
+++ templates/content-lesson/block-content.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template for displaying block lesson content.
+ *
+ * This template can be overridden by copying it to yourtheme/learnpress/content-lesson/block-content.php.
+ *
+ * @author  ThimPress
+ * @package  Learnpress/Templates
+ * @version  3.0.0
+ */
+
+/**
+ * Prevent loading this file directly
+ */
+defined( 'ABSPATH' ) || exit();
+
+learn_press_display_message( __( 'This lesson has been locked', 'learnpress' ), 'error' );
