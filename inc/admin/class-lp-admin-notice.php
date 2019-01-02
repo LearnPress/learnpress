f2bf6c5e8aaf4204c143329ede139a4bd557b638
tunnhn <tunnhn@gmail.com>
2019-01-02 15:46:57 +0700

    Dismiss notice api

--- inc/admin/class-lp-admin-notice.php
+++ inc/admin/class-lp-admin-notice.php
@@ -6,7 +6,7 @@
  * @author     ThimPress
  * @version    1.0
  */
-if ( !defined( 'ABSPATH' ) ) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
@@ -21,16 +21,123 @@ class LP_Admin_Notice {
 	protected static $_notices = array();
 
 	/**
+	 * @since 3.x.x
+	 *
+	 * @var LP_Admin_Notice
+	 */
+	protected static $instance = false;
+
+	/**
 	 * LP_Admin_Notice construct
 	 */
-	public function __construct() {
+	protected function __construct() {
 		add_action( 'init', array( $this, 'dismiss_notice' ) );
 		add_action( 'admin_notices', array( __CLASS__, 'show_notices' ), 100000 );
 	}
 
+	/**
+	 * Update option to turn-off a notice.
+	 *
+	 * @since 3.x.x
+	 *
+	 * @param string $name
+	 * @param string $value
+	 * @param int    $expired
+	 */
+	public function dismiss_notice_2( $name, $value, $expired = 0 ) {
+		if ( $expired ) {
+			set_transient( 'lp_dismiss_notice' . $name, $value, $expired );
+		} else {
+			$values = get_option( 'lp_dismiss_notice' );
+			if ( ! $values ) {
+				$values = array( $name => $value );
+			} else {
+				$values[ $name ] = $value;
+			}
+
+			update_option( 'lp_dismiss_notice', $values );
+		}
+	}
+
+	/**
+	 * Check if a notice has dismissed.
+	 *
+	 * @since 3.x.x
+	 *
+	 * @param string $name
+	 *
+	 * @return bool
+	 */
+	public function has_dismissed_notice( $name ) {
+		if ( $transient = get_transient( 'lp_dismiss_notice' . $name ) ) {
+			return $transient;
+		}
+
+		$values = get_option( 'lp_dismiss_notice' );
+		if ( ! $values ) {
+			return false;
+		}
+
+		return isset( $values[ $name ] ) ? $values[ $name ] : false;
+	}
+
+	/**
+	 * Remove a notice has been dismissed.
+	 *
+	 * @since 3.x.x
+	 *
+	 * @param string|array $name    - Optional. NULL will remove all notices.
+	 * @param bool         $expired - Optional. TRUE if dismiss notice as transient (in case $name passed).
+	 *
+	 * @return bool
+	 */
+	public function remove_dismissed_notice( $name = '' ) {
+
+		if ( ! $name ) {
+			global $wpdb;
+
+			$query = $wpdb->prepare( "SELECT SUBSTR(option_name, 12) FROM {$wpdb->options} WHERE option_name LIKE %s", '%' . $wpdb->esc_like( '_transient_lp_dismiss_notice' ) . '%' );
+
+			if ( $all_notices = $wpdb->get_col( $query ) ) {
+				foreach ( $all_notices as $notice ) {
+					delete_transient( $notice );
+				}
+			}
+
+			delete_option( 'lp_dismiss_notice' );
+
+			return true;
+		} elseif ( is_array( $name ) ) {
+			foreach ( $name as $notice ) {
+				$this->remove_dismissed_notice( $notice );
+			}
+
+			return true;
+		}
+
+		delete_transient( 'lp_dismiss_notice' . $name );
+
+		$values = get_option( 'lp_dismiss_notice' );
+
+		if ( ! $values ) {
+			return false;
+		} else {
+			if ( array_key_exists( $name, $values ) ) {
+				unset( $values[ $name ] );
+
+				update_option( 'lp_dismiss_notice', $values );
+
+				return $values;
+			}
+		}
+
+		return false;
+	}
+
 	public function dismiss_notice() {
+
 		$notice = learn_press_get_request( 'lp-hide-notice' );
-		if ( !$notice ) {
+		if ( ! $notice ) {
 			return;
 		}
 		if ( $transient = learn_press_get_request( 't' ) ) {
@@ -100,6 +207,21 @@ class LP_Admin_Notice {
 			delete_transient( 'learn_press_redirect_notices' );
 		}
 	}
+
+	/**
+	 * Get single instance of this class
+	 *
+	 * @since 3.x.x
+	 *
+	 * @return LP_Admin_Notice
+	 */
+	public static function instance() {
+		if ( ! self::$instance ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
 }
 
-new LP_Admin_Notice();
+LP_Admin_Notice::instance();
