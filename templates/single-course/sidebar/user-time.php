1351a0b3192812fa67cdb2d4a83a531ea19af75c
tunnhn <tunnhn@gmail.com>
2019-12-04 15:47:23 +0700

    Added class for course time

--- templates/single-course/sidebar/user-time.php
+++ templates/single-course/sidebar/user-time.php
@@ -20,26 +20,26 @@ $user   = LP_Global::user();
 $course = LP_Global::course();
 ?>
 <div class="course-time">
-    <p>
+    <p class="course-time-row">
         <strong><?php esc_html_e( 'You started on:', 'learnpress' ); ?></strong>
 		<?php echo $start_time->format( 'Y-m-d H:i:s' ); ?>
     </p>
 	<?php if ( $status === 'enrolled' ) { ?>
 		<?php if ( $expiration_time ) { ?>
-            <p>
+            <p class="course-time-row">
                 <strong><?php esc_html_e( 'You will end:', 'learnpress' ); ?></strong>
 				<?php echo $expiration_time->format( 'i18n' ); ?>
             </p>
 		<?php } else { ?>
-            <p>
+            <p class="course-time-row">
                 <strong><?php esc_html_e( 'Course access:', 'learnpress' ); ?></strong>
 				<?php esc_html_e( 'Lifetime', 'learnpress' ); ?>
             </p>
 		<?php } ?>
 	<?php } elseif ( $status === 'finished' && $end_time ) { ?>
-		<p>
-			<strong><?php esc_html_e( 'You ended:', 'learnpress' ); ?></strong>
-			<?php echo $end_time->format('human'); ?>
-		</p>
+        <p class="course-time-row">
+            <strong><?php esc_html_e( 'You ended:', 'learnpress' ); ?></strong>
+			<?php echo $end_time->format( 'human' ); ?>
+        </p>
 	<?php } ?>
 </div>
