b5fc0560b461911f8c9047fadbd9dce737fbde4e
daonham <daonham95@gmail.com>
2020-11-11 14:59:23 +0700

    fix: enroll course in checkout

--- inc/admin/views/setup/notice-setup.php
+++ inc/admin/views/setup/notice-setup.php
@@ -1,9 +1,7 @@
 <div id="notice-install" class="lp-notice notice notice-info">
 	<p><?php _e( '<strong>LearnPress LMS is ready to use.</strong>', 'learnpress' ); ?></p>
 	<p>
-		<a class="button button-primary"
-		   href="<?php echo admin_url( 'index.php?page=lp-setup' ); ?>"><?php _e( 'Quick Setup', 'learnpress' ); ?></a>
-<!--        <button class="button" id="skip-notice-install">--><?php // _e( 'Skip', 'learnpress' ); ?><!--</button>-->
+		<a class="button button-primary" href="<?php echo admin_url( 'index.php?page=lp-setup' ); ?>"><?php _e( 'Quick Setup', 'learnpress' ); ?></a>
 		<button class="button" data-dismiss-notice="skip-setup-wizard"><?php _e( 'Skip', 'learnpress' ); ?></button>
 	</p>
 </div>
