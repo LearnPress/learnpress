24fa4fc7e8a3da8b43a94dd2a83e1e23b82614ff
daonham <daonham95@gmail.com>
2021-01-05 08:51:13 +0700

    fix: WP Bakery with Quiz

--- assets/src/apps/js/frontend/single-curriculum/components/compatible.js
+++ assets/src/apps/js/frontend/single-curriculum/components/compatible.js
@@ -5,33 +5,54 @@
  */
 
 LP.Hook.addAction( 'lp-compatible-builder', () => {
-	if ( typeof elementorFrontend !== 'undefined' ) {
-		LP.Hook.removeAction( 'lp-compatible-builder' );
+	LP.Hook.removeAction( 'lp-compatible-builder' );
 
+	if ( typeof elementorFrontend !== 'undefined' ) {
 		[ ...document.querySelectorAll( '#popup-content' ) ][ 0 ].addEventListener( 'scroll', () => {
 			Waypoint.refreshAll();
 			window.dispatchEvent( new Event( 'resize' ) );
 		} );
 	}
+
+	if ( typeof vc_js !== 'undefined' ) {
+		[ ...document.querySelectorAll( '#popup-content' ) ][ 0 ].addEventListener( 'scroll', () => {
+			VcWaypoint.refreshAll();
+		} );
+	}
 } );
 
 LP.Hook.addAction( 'lp-quiz-compatible-builder', () => {
+	LP.Hook.removeAction( 'lp-quiz-compatible-builder' );
+
+	LP.Hook.doAction( 'lp-compatible-builder' );
+
 	if ( typeof elementorFrontend !== 'undefined' ) {
-		LP.Hook.removeAction( 'lp-quiz-compatible-builder' );
-
-		LP.Hook.doAction( 'lp-compatible-builder' );
-
 		return window.elementorFrontend.init();
+	}
+
+	if ( typeof vc_js !== 'undefined' ) {
+		vc_round_charts();
+		vc_pieChart();
+		vc_line_charts();
+
+		return window.vc_js();
 	}
 } );
 
 LP.Hook.addAction( 'lp-question-compatible-builder', () => {
+	LP.Hook.removeAction( 'lp-question-compatible-builder' );
+	LP.Hook.removeAction( 'lp-quiz-compatible-builder' );
+	LP.Hook.doAction( 'lp-compatible-builder' );
+
 	if ( typeof elementorFrontend !== 'undefined' ) {
-		LP.Hook.removeAction( 'lp-question-compatible-builder' );
-		LP.Hook.removeAction( 'lp-quiz-compatible-builder' );
-
-		LP.Hook.doAction( 'lp-compatible-builder' );
-
 		return window.elementorFrontend.init();
+	}
+
+	if ( typeof vc_js !== 'undefined' ) {
+		vc_round_charts();
+		vc_pieChart();
+		vc_line_charts();
+
+		return window.vc_js();
 	}
 } );
