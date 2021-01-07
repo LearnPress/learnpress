33c13a47f5008c40308cd6efd369ecf3ca779bfb
hungkieu190 <=>
2021-01-07 14:15:46 +0700

    reverse file class-lp-admin-dashboard

--- inc/admin/class-lp-admin-dashboard.php
+++ inc/admin/class-lp-admin-dashboard.php
@@ -34,7 +34,80 @@ if ( ! class_exists( 'LP_Admin_Dashboard' ) ) {
 
 			?>
 
-            <ul class="lp-order-statuses"> </ul>
+            <ul class="lp-order-statuses">
+                <li class="count-number total-raised">
+                    <strong><?php echo $this->_get_order_total_raised(); ?></strong>
+                    <p><?php _e( 'Total Raised', 'learnpress' ); ?></p>
+                </li>
+				<?php
+
+				$order_statuses    = learn_press_get_order_statuses( true, true );
+				$specific_statuses = array( 'lp-completed', 'lp-failed'/*, 'lp-on-hold'*/ );
+
+				foreach ( $order_statuses as $status ) {
+					if ( ! in_array( $status, $specific_statuses ) ) {
+						$specific_statuses[] = $status;
+					}
+				}
+
+				$counts = learn_press_count_orders( array( 'status' => $specific_statuses ) );
+
+				foreach ( $specific_statuses as $status ) :
+					$status_object = get_post_status_object( $status );
+					if ( ! $status_object ) {
+						continue;
+					}
+					$count = $counts[ $status ];
+					$url   = $count ? admin_url( 'edit.php?post_type=' . LP_ORDER_CPT . '&post_status=' . $status ) : '#';
+					?>
+                    <li class="counter-number order-<?php echo str_replace( 'lp-', '', $status ); ?>">
+                        <div class="counter-inner">
+                            <a href="<?php echo esc_url( $url ); ?>">
+                                <strong>
+									<?php if ( $count ) {
+										printf( translate_nooped_plural( _n_noop( '%d order', '%d orders' ), $count, 'learnpress' ), $count );
+									} else {
+										printf( __( '%d order', 'learnpress' ), 0 );
+									} ?>
+                                </strong>
+                                <p><?php printf( '%s', $status_object->label ); ?></p>
+                            </a>
+                        </div>
+                    </li>
+				<?php endforeach; ?>
+				<?php
+				$eduma_data = $this->_get_theme_info( 14058034 );
+
+				if ( ! empty( $eduma_data ) ) {
+					?>
+                    <li class="clear"></li>
+                    <li class="featured-theme">
+                        <p>
+                            <a href="<?php echo esc_url( $eduma_data['url'] ) ?>">
+								<?php echo esc_html( $eduma_data['name'] ) ?>
+                            </a> - <?php printf( '%s%s', '$', $eduma_data['price_cents'] / 100 ); ?>
+                        </p>
+                        <div>
+
+							<?php wp_star_rating( array(
+								'rating' => $eduma_data['rating']['rating'],
+								'type'   => 'rating',
+								'number' => $eduma_data['rating']['count']
+							) ); ?>
+                            <span class="count-rating">(<?php echo $eduma_data['rating']['count']; ?>)</span>
+                            <span>
+                                - <?php echo $eduma_data['number_of_sales'] . __( ' sales', 'learnpress' ); ?>
+                            </span>
+                        </div>
+                        <p>
+							<?php _e( 'Created by: ', 'learnpress' ) ?>
+                            <a href="https://thimpress.com/"
+                               class="author"><?php echo esc_html( $eduma_data['author_username'] ); ?></a>
+                        </p>
+                    </li>
+
+				<?php } ?>
+            </ul>
 			<?php
 		}
 
