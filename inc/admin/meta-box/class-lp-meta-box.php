f5e25fa695e10a98f77e9f721d31ac2c750be43f
tungnx <tungnxt89@gmail.com>
2021-02-03 11:45:11 +0700

    = 4.0.0 = ~ Modify class-lp-meta-box.php file

--- inc/admin/meta-box/class-lp-meta-box.php
+++ inc/admin/meta-box/class-lp-meta-box.php
@@ -10,9 +10,19 @@ class LP_Meta_Box {
 
 	private static $saved_meta_boxes = false;
 
+	/**
+	 * LP_Meta_Box constructor.
+	 *
+	 * @see LP_Meta_Box_Course::save
+	 * @see LP_Meta_Box_Lesson::save
+	 * @see LP_Meta_Box_Question::save
+	 * @see LP_Meta_Box_Quiz::save
+	 * @see LP_Meta_Box_Course::save_eduma_child_metabox_v3
+	 */
 	public function __construct() {
 		add_action( 'save_post', array( $this, 'save_meta_boxes' ), 100, 2 );
 
+		add_action( 'learnpress_save_lp_course_metabox', 'LP_Meta_Box_Course::save' );
 		add_action( 'learnpress_save_lp_lesson_metabox', 'LP_Meta_Box_Lesson::save' );
 		add_action( 'learnpress_save_lp_question_metabox', 'LP_Meta_Box_Question::save' );
 		add_action( 'learnpress_save_lp_quiz_metabox', 'LP_Meta_Box_Quiz::save' );
@@ -20,7 +30,7 @@ class LP_Meta_Box {
 	}
 
 	/**
-	 * @param id $post_id
+	 * @param id      $post_id
 	 * @param WP_Post $post
 	 */
 	public function save_meta_boxes( $post_id = 0, $post = null ) {
@@ -44,8 +54,6 @@ class LP_Meta_Box {
 		}
 
 		self::$saved_meta_boxes = true;
-
-		LP_Meta_Box_Course::save( $post_id );
 
 		do_action( 'learnpress_save_' . $post->post_type . '_metabox', $post_id, $post );
 	}
