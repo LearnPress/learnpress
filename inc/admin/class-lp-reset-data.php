0317f08ac9485b355bf5afaaf7c7bc1a5932eaa4
hungkieu190 <hungkieu190@gmail.com>
2021-04-24 10:32:45 +0700

    fix tool reset user progress

--- inc/admin/class-lp-reset-data.php
+++ inc/admin/class-lp-reset-data.php
@@ -243,46 +243,25 @@ class LP_Reset_Data {
 		$user_id   = LP_Request::get_int( 'user_id' );
 		$course_id = LP_Request::get_int( 'course_id' );
 
-		global $wpdb;
-		if ( $course_id ) {
+		$user = learn_press_get_user($user_id);
+		$user_course_data = $user->get_course_data( $course_id );
+		// Set status, start_time, end_time of course to enrolled.
+		$user_course_data->set_status( LP_COURSE_ENROLLED )
+		                 ->set_start_time( current_time( 'mysql', true ) )
+		                 ->set_end_time( '' )
+		                 ->set_graduation( 'in-progress' )
+		                 ->update();
 
-			self::reset_course_users( $course_id, $user_id );
+		// Remove items' course user learned.
+		$filter_remove            = new LP_User_Items_Filter();
+		$filter_remove->parent_id = $user_course_data->get_user_item_id();
+		$filter_remove->user_id   = $user_course_data->get_user_id();
+		$filter_remove->limit     = - 1;
+		LP_User_Items_DB::getInstance()->remove_items_of_user_course( $filter_remove );
 
-		} else {
+		// Create new result in table learnpress_user_item_results.
+		LP_User_Items_Result_DB::instance()->insert( $user_course_data->get_user_item_id() );
 
-			$query = $wpdb->prepare(
-				"
-				SELECT user_item_id
-				FROM {$wpdb->learnpress_user_items}
-				WHERE user_id = %d
-			",
-				$user_id
-			);
-
-			$user_item_ids = $wpdb->get_col( $query );
-			if ( $user_item_ids ) {
-				$format = array_fill( 0, sizeof( $user_item_ids ), '%d' );
-				$query  = $wpdb->prepare(
-					"
-					DELETE
-					FROM {$wpdb->learnpress_user_itemmeta}
-					WHERE learnpress_user_item_id IN(" . join( ',', $format ) . ')
-				',
-					$user_item_ids
-				);
-				$wpdb->query( $query );
-
-				$query = $wpdb->prepare(
-					"
-					DELETE
-					FROM {$wpdb->learnpress_user_items}
-					WHERE user_id = %d
-				",
-					$user_id
-				);
-				$wpdb->query( $query );
-			}
-		}
 		die();
 	}
 
