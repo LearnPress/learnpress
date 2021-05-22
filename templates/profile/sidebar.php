2e58948ece5dd19b16e45648f03e1e06a34d634d
Tuan Tran <anhtuan.ictu@gmail.com>
2021-05-22 14:58:02 +0700

    fix login not show in page profile

--- templates/profile/sidebar.php
+++ templates/profile/sidebar.php
@@ -10,6 +10,12 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 
+<div class="wrapper-profile-header wrap-fullwidth">
+	<div class="lp-content-area lp-profile-content-area">
+ 			<?php do_action( 'learn-press/user-profile-account' ); ?>
+ 	</div>
+</div>
+
 <aside id="profile-sidebar">
 
 	<?php do_action( 'learn-press/user-profile-tabs' ); ?>
