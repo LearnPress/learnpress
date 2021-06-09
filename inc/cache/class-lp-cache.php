5b7569ba34d070ddfafcffd8cea0361aa2c7478b
tungnx <tungnxt89@gmail.com>
2021-06-09 17:13:16 +0700

    = 4.0.8 = ~ Add file class-lp-cache.php

---
+++ inc/cache/class-lp-cache.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class LP_Cache
+ *
+ * @author tungnx
+ * @since 4.0.8
+ * @version 1.0.0
+ */
+defined( 'ABSPATH' ) || exit();
+
+class LP_Cache {
+	public function __construct() {
+
+	}
+}
