222919584ad7862c896035283f24493c6d7e4a91
Tuan Tran <anhtuan.ictu@gmail.com>
2021-07-17 10:50:14 +0700

    change name class backcourse

--- templates/single-course/content-item/popup-header.php
+++ templates/single-course/content-item/popup-header.php
@@ -39,5 +39,5 @@ $percentage     = $course_results['count_items'] ? absint( $course_results['comp
 			</div>
 		<?php endif; ?>
  	</div>
-	<a href="<?php echo $course->get_permalink(); ?>" class="back_course"><i class="fa fa-times"></i></a>
+	<a href="<?php echo $course->get_permalink(); ?>" class="back-course"><i class="fa fa-times"></i></a>
  </div>
