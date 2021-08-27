49193eed5a2c97850e9ce9c6eb40abbc7f841c48
tungnx <tungnxt89@gmail.com>
2021-08-27 15:13:04 +0700

    = 4.1.3 =

--- learnpress.php
+++ learnpress.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://thimpress.com/learnpress
  * Description: LearnPress is a WordPress complete solution for creating a Learning Management System (LMS). It can help you to create courses, lessons and quizzes.
  * Author: ThimPress
- * Version: 5.0.0-beta-5
+ * Version: 4.1.3-beta-5
  * Author URI: http://thimpress.com
  * Requires at least: 5.6
  * Tested up to: 5.7
