a2f11cea2288f424ffce00022edcf040dd30a2e9
tungnx <tungnxt89@gmail.com>
2021-12-16 03:32:47 +0700

    = 4.1.4.1 = ~ Fixed minor bug

--- assets/src/apps/js/frontend/quiz/store/actions.js
+++ assets/src/apps/js/frontend/quiz/store/actions.js
@@ -131,9 +131,9 @@ export function* submitQuiz() {
 		return;
 	}
 
-	const answered = getQuestionsSelectedAnswers();
+	let answered = getQuestionsSelectedAnswers();
 
-	if ( Object.keys( answered ).length === 0 && lpQuizSettings.checkNorequizenroll == '1' ) {
+	if ( Object.keys( answered ).length === 0 && lpQuizSettings.checkNorequizenroll === '1' ) {
 		const data = JSON.parse( localStorage.getItem( `LP_Quiz_${ itemId }_Answered` ) );
 
 		for ( const [ k, v ] of Object.entries( data ) ) {
