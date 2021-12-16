fabad11145333045f5c61fc836e72c8793ca4e62
daonham <daonham95@gmail.com>
2021-12-16 14:40:15 +0700

    new: update package

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	extends: [ 'stylelint-config-wordpress/scss', 'prettier-stylelint/index.js' ],
+	extends: [ '@wordpress/stylelint-config', 'prettier-stylelint/index.js' ],
 	plugins: 'stylelint-order',
 	rules: {
 		'max-line-length': 255,
