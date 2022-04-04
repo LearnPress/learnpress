fe371a5772cc6b6997831b544a69f5af806ac62d
tungnx <tungnxt89@gmail.com>
2022-04-04 16:45:24 +0700

    = 1.0.0 =

--- inc/admin/views/setup/steps/finish.php
+++ inc/admin/views/setup/steps/finish.php
@@ -14,18 +14,21 @@ defined( 'ABSPATH' ) or exit;
 <p><?php _e( 'LearnPress LMS is ready to go!', 'learnpress' ); ?></p>
 
 <p class="finish-buttons">
-    <a class="button"
-       id="install-sample-course"
-       href="<?php echo esc_url( wp_nonce_url( admin_url( 'index.php?page=lp-install-sample-data' ), 'install-sample-course' ) ); ?>">
+	<a class="button"
+		id="install-sample-course"
+		href="<?php echo esc_url( admin_url( 'admin.php?page=learn-press-tools' ) ); ?>">
 		<?php _e( 'Install a demo course', 'learnpress' ); ?>
-    </a>
+	</a>
 
-    <a class="button"
-       href="https://docspress.thimpress.com/admin-learnpress-4-0/"><?php _e( 'View Documentation', 'learnpress' ); ?></a>
+	<a class="button" href="https://docspress.thimpress.com/admin-learnpress-4-0/">
+		<?php _e( 'View Documentation', 'learnpress' ); ?>
+	</a>
 
-    <a class="button"
-       href="<?php echo esc_url( admin_url( 'post-new.php?post_type=lp_course' ) ); ?>"><?php _e( 'Create a new course', 'learnpress' ); ?></a>
+	<a class="button" href="<?php echo esc_url( admin_url( 'post-new.php?post_type=lp_course' ) ); ?>">
+		<?php _e( 'Create a new course', 'learnpress' ); ?>
+	</a>
 
-    <a class="button"
-       href="<?php echo esc_url( admin_url( 'index.php' ) ); ?>"><?php _e( 'Back to Dashboard', 'learnpress' ); ?></a>
+	<a class="button" href="<?php echo esc_url( admin_url( 'index.php' ) ); ?>">
+		<?php _e( 'Back to Dashboard', 'learnpress' ); ?>
+	</a>
 </p>
