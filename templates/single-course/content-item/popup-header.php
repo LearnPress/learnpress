9eb600707e7ad88702909e3fd1421158f8a0cb2b
tungnx <tungnxt89@gmail.com>
2022-07-26 10:21:56 +0700

    ~ Modified: popup-header.php

--- templates/single-course/content-item/popup-header.php
+++ templates/single-course/content-item/popup-header.php
@@ -20,20 +20,20 @@ if ( ! isset( $course ) || ! isset( $user ) || ! isset( $percentage ) ||
 <div id="popup-header">
 	<div class="popup-header__inner">
 		<h2 class="course-title">
-			<a href="<?php echo esc_url_raw( $course->get_permalink() ); ?>"><?php echo $course->get_title(); ?></a>
+			<a href="<?php echo esc_url_raw( $course->get_permalink() ); ?>"><?php echo wp_kses_post( $course->get_title() ); ?></a>
 		</h2>
 
 		<?php if ( $user->has_enrolled_or_finished( $course->get_id() ) ) : ?>
-			<div class="items-progress" data-total-items="<?php echo $total_items; ?>">
+			<div class="items-progress" data-total-items="<?php echo esc_attr( $total_items ); ?>">
 				<span class="number">
 					<?php echo wp_sprintf( '<span class="items-completed">%1$s</span> of %2$d %3$s', esc_html( $completed_items ), esc_html( $course->count_items() ), __( 'items', 'learnpress' ) ); ?>
 				</span>
 				<div class="learn-press-progress">
-					<div class="learn-press-progress__active" data-value="<?php echo $percentage; ?>%;">
+					<div class="learn-press-progress__active" data-value="<?php echo esc_attr( $percentage ); ?>%;">
 					</div>
 				</div>
 			</div>
 		<?php endif; ?>
 	</div>
-	<a href="<?php echo $course->get_permalink(); ?>" class="back-course"><i class="fa fa-times"></i></a>
+	<a href="<?php echo esc_url_raw( $course->get_permalink() ); ?>" class="back-course"><i class="fa fa-times"></i></a>
 </div>
