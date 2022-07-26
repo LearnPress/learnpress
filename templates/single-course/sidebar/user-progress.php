1cc799e3c62e54986bf05c59c7524af737cbca8c
tungnx <tungnxt89@gmail.com>
2022-07-26 11:03:02 +0700

    ~ Modified: user-progress.php

--- templates/single-course/sidebar/user-progress.php
+++ templates/single-course/sidebar/user-progress.php
@@ -13,14 +13,6 @@ if ( ! isset( $user ) || ! isset( $course ) || ! isset( $course_data ) || ! isse
 	return;
 }
 
-/*if ( ! isset( $course_data ) ) {
-	$course_data = $user->get_course_data( $course->get_id() );
-}
-
-if ( ! isset( $course_results ) ) {
-	$course_results = $course_data->calculate_course_results();
-}*/
-
 $passing_condition = $course->get_passing_condition();
 $quiz_false        = 0;
 
@@ -37,7 +29,7 @@ if ( ! empty( $course_results['items'] ) ) {
 			<h4 class="items-progress__heading">
 				<?php esc_html_e( 'Lessons completed:', 'learnpress' ); ?>
 			</h4>
-			<span class="number"><?php printf( '%1$d/%2$d', $course_results['items']['lesson']['completed'], $course_results['items']['lesson']['total'] ); ?></span>
+			<span class="number"><?php esc_attr( printf( '%1$d/%2$d', $course_results['items']['lesson']['completed'], $course_results['items']['lesson']['total'] ) ); ?></span>
 		</div>
 	<?php endif; ?>
 
@@ -48,7 +40,7 @@ if ( ! empty( $course_results['items'] ) ) {
 			<h4 class="items-progress__heading">
 				<?php esc_html_e( 'Quizzes finished:', 'learnpress' ); ?>
 			</h4>
-			<span class="number" title="<?php esc_attr( sprintf( __( 'Failed %1$d, Passed %2$d', 'learnpress' ), $quiz_false, $course_results['items']['quiz']['passed'] ) ); ?>"><?php printf( __( '%1$d/%2$d', 'learnpress' ), $course_results['items']['quiz']['completed'], $course_results['items']['quiz']['total'] ); ?></span>
+			<span class="number" title="<?php echo esc_attr( sprintf( __( 'Failed %1$d, Passed %2$d', 'learnpress' ), $quiz_false, $course_results['items']['quiz']['passed'] ) ); ?>"><?php printf( __( '%1$d/%2$d', 'learnpress' ), $course_results['items']['quiz']['completed'], $course_results['items']['quiz']['total'] ); ?></span>
 		</div>
 	<?php endif; ?>
 
@@ -60,18 +52,20 @@ if ( ! empty( $course_results['items'] ) ) {
 		</h4>
 
 		<div class="lp-course-status">
-			<span class="number"><?php echo $course_results['result']; ?><span class="percentage-sign">%</span></span>
+			<span class="number"><?php echo esc_html( $course_results['result'] ); ?><span class="percentage-sign">%</span></span>
 		</div>
 
-		<div class="learn-press-progress lp-course-progress <?php echo $course_data->is_passed() ? ' passed' : ''; ?>"
-			data-value="<?php echo $course_results['result']; ?>"
-			data-passing-condition="<?php echo $passing_condition; ?>"
+		<div class="learn-press-progress lp-course-progress <?php echo esc_attr( $course_data->is_passed() ? ' passed' : '' ); ?>"
+			data-value="<?php echo esc_attr( $course_results['result'] ); ?>"
+			data-passing-condition="<?php echo esc_attr( $passing_condition ); ?>"
 			title="<?php echo esc_attr( learn_press_translate_course_result_required( $course ) ); ?>">
 			<div class="progress-bg lp-progress-bar">
-				<div class="progress-active lp-progress-value" style="left: <?php echo $course_results['result']; ?>%;">
+				<div class="progress-active lp-progress-value" style="left: <?php echo esc_attr( $course_results['result'] ); ?>%;">
 				</div>
 			</div>
-			<div class="lp-passing-conditional" data-content="<?php printf( esc_html__( 'Passing condition: %s%%', 'learnpress' ), $passing_condition ); ?>" style="left: <?php echo $passing_condition; ?>%;">
+			<div class="lp-passing-conditional"
+				data-content="<?php esc_attr( printf( esc_html__( 'Passing condition: %s%%', 'learnpress' ), $passing_condition ) ); ?>"
+				style="left: <?php echo esc_attr( $passing_condition ); ?>%;">
 			</div>
 		</div>
 	</div>
