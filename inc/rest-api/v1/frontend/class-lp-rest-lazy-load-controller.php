fdbab768866fb0aa96d417f51da7382dfbaf9a5b
tungnx <tungnxt89@gmail.com>
2023-01-17 18:09:14 +0700

    = 4.2.1 =

--- inc/rest-api/v1/frontend/class-lp-rest-lazy-load-controller.php
+++ inc/rest-api/v1/frontend/class-lp-rest-lazy-load-controller.php
@@ -166,7 +166,7 @@ class LP_REST_Lazy_Load_Controller extends LP_Abstract_REST_Controller {
 		$content  = '';
 
 		$course_id  = absint( $params['courseId'] ?? 0 );
-		$per_page   = LP_Settings::instance()->get( 'section_per_page', 2 );
+		$per_page   = LP_Settings::instance()->get( 'section_per_page', 1 );
 		$page       = absint( $params['page'] ?? 1 );
 		$order      = wp_unslash( $params['order'] ?? 'ASC' );
 		$search     = wp_unslash( $params['search'] ?? '' );
@@ -253,7 +253,7 @@ class LP_REST_Lazy_Load_Controller extends LP_Abstract_REST_Controller {
 		$params = $request->get_params();
 
 		$section_id = absint( $params['sectionId'] ?? 0 );
-		$per_page   = LP_Settings::instance()->get( 'course_item_per_page', 5 );
+		$per_page   = LP_Settings::instance()->get( 'course_item_per_page', 10 );
 		$page       = absint( $params['page'] ?? 1 );
 		$order      = LP_Helper::sanitize_params_submitted( $params['order'] ?? 'ASC' );
 		$search     = LP_Helper::sanitize_params_submitted( $params['search'] ?? '' );
