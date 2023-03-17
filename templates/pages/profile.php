975b4354e6d7144345eb2efbc742ce864c08f31f
tungnx <tungnxt89@gmail.com>
2023-03-17 18:01:35 +0700

    = 4.2.2.3 = ~ Clear cache LP Settings when update option LP. ~ Modified: shortcode LP Profile.

--- templates/pages/profile.php
+++ templates/pages/profile.php
@@ -13,29 +13,28 @@ if ( ! isset( $profile ) ) {
 	return;
 }
 ?>
+	<div id="learn-press-profile" <?php $profile->main_class(); ?>>
+		<?php if ( $profile->is_public() || $profile->get_user()->is_guest() ) : ?>
 
-<div id="learn-press-profile" <?php $profile->main_class(); ?>>
-	<?php if ( $profile->is_public() || $profile->get_user()->is_guest() ) : ?>
+			<?php do_action( 'learn-press/before-user-profile', $profile ); ?>
 
-		<?php do_action( 'learn-press/before-user-profile', $profile ); ?>
+			<div class="lp-content-area">
+				<?php
+				if ( ! is_user_logged_in() ) {
+					learn_press_print_messages( true );
+				}
 
-		 <div class="lp-content-area">
-			<?php
-			if ( ! is_user_logged_in() ) {
-				learn_press_print_messages( true );
-			}
-
-			/**
-			 * @since 3.0.0
-			 */
-			do_action( 'learn-press/user-profile', $profile );
-			?>
-		</div>
-	<?php else : ?>
-		<div class="lp-content-area">
-			<?php esc_html_e( 'This user does not make their profile public.', 'learnpress' ); ?>
-		</div>
-	<?php endif; ?>
+				/**
+				 * @since 3.0.0
+				 */
+				do_action( 'learn-press/user-profile', $profile );
+				?>
+			</div>
+		<?php else : ?>
+			<div class="lp-content-area">
+				<?php esc_html_e( 'This user does not make their profile public.', 'learnpress' ); ?>
+			</div>
+		<?php endif; ?>
 
 	</div>
 <?php
