918ef0ed5f00765bfcb8266f8c71885dd76fbfdc
tungnx <tungnxt89@gmail.com>
2023-03-20 08:55:48 +0700

    = 4.2.2.3 = ~ Comment class handle block Gutenberg.

--- learnpress.php
+++ learnpress.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://thimpress.com/learnpress
  * Description: LearnPress is a WordPress complete solution for creating a Learning Management System (LMS). It can help you to create courses, lessons and quizzes.
  * Author: ThimPress
- * Version: 4.2.2.3-beta-1
+ * Version: 4.2.2.3-beta-2
  * Author URI: http://thimpress.com
  * Requires at least: 5.8
  * Tested up to: 6.1.1
@@ -383,8 +383,8 @@ if ( ! class_exists( 'LearnPress' ) ) {
 			require_once 'inc/cart/lp-cart-functions.php';
 
 			// Block Templates
-			require_once 'inc/block-template/class-block-until.php';
-			require_once 'inc/block-template/class-block-controller.php';
+			//require_once 'inc/block-template/class-block-until.php';
+			//require_once 'inc/block-template/class-block-controller.php';
 
 			// API
 			require_once 'inc/abstracts/abstract-rest-api.php';
