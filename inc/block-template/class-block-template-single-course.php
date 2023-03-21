1940f54413dfaeafdfaf438695f3790e71030f6c
DoNgocPhuc <phucngocdo@gmail.com>
2023-03-21 14:07:19 +0700

    block template config

--- inc/block-template/class-block-template-single-course.php
+++ inc/block-template/class-block-template-single-course.php
@@ -7,27 +7,27 @@
  */
 class Block_Template_Single_Course extends Abstract_Block_Template
 {
-	public $slug = 'single-lp_course';
-	public $name = 'learnpress/single-course';
-	public $title = 'Single Course (LearnPress)';
-	public $description = 'Single Course Block Template';
-	public $path_html_block_template_file = 'html/single-lp_course.html';
+    public $slug = 'single-lp_course';
+    public $name = 'learnpress/single-course';
+    public $title = 'Single Course (LearnPress)';
+    public $description = 'Single Course Block Template';
+    public $path_html_block_template_file = 'html/single-lp_course.html';
 
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	/**
-	 * Render content of block tag
-	 *
-	 * @param array $attributes | Attributes of block tag.
-	 *
-	 * @return false|string
-	 */
-	public function render_content_block_template(array $attributes)
-	{
+    /**
+     * Render content of block tag
+     *
+     * @param array $attributes | Attributes of block tag.
+     *
+     * @return false|string
+     */
+    public function render_content_block_template(array $attributes)
+    {
 //      Debug::var_dump($attributes);
-		return parent::render_content_block_template($attributes);
-	}
+        return parent::render_content_block_template($attributes);
+    }
 }
