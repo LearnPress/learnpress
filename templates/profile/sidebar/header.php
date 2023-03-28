bea86243b5944b7ab2cad1c45a717117c59b36f5
DoNgocPhuc <phucngocdo@gmail.com>
2023-03-28 14:51:05 +0700

    user profile

---
+++ templates/profile/sidebar/header.php
@@ -0,0 +1,7 @@
+<?php
+?>
+<div class="wrapper-profile-header wrap-fullwidth">
+	<div class="lp-content-area lp-profile-content-area">
+		<?php do_action( 'learn-press/user-profile-account' ); ?>
+	</div>
+</div>
---
+++ templates/profile/sidebar/sidebar.php
@@ -0,0 +1,6 @@
+<?php
+?>
+
+<aside id="profile-sidebar">
+    <?php do_action('learn-press/user-profile-tabs'); ?>
+</aside>
