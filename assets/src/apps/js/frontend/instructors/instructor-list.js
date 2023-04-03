0ed6b83c609dcbcc54cb3d1687c000cdbb6e00f1
DoNgocPhuc <phucngocdo@gmail.com>
2023-04-03 16:06:00 +0700

    fix instructor url

--- assets/src/apps/js/frontend/instructors/instructor-list.js
+++ assets/src/apps/js/frontend/instructors/instructor-list.js
@@ -1,93 +1,93 @@
-import {addQueryArgs} from '@wordpress/url';
+import { addQueryArgs } from '@wordpress/url';
 
-const instructorNode = document.querySelector('.lp-instructors');
+const instructorNode = document.querySelector( '.lp-instructors' );
 let contentAreaNode, containerNode;
 let query = {};
 
 export default function InstructorList() {
-	if (!instructorNode) {
+	if ( ! instructorNode ) {
 		return;
 	}
-	contentAreaNode = document.querySelector('.lp-content-area');
-	containerNode = document.querySelector('.lp-instructor-list');
-	const initDataNode = contentAreaNode.querySelector('input[name="init-data"]');
-	query = {paged: 1};
-	query = {...query,...JSON.parse(initDataNode.value)}
+	contentAreaNode = document.querySelector( '.lp-content-area' );
+	containerNode = document.querySelector( '.lp-instructor-list' );
+	const initDataNode = contentAreaNode.querySelector( 'input[name="init-data"]' );
+	query = { paged: 1 };
+	query = { ...query, ...JSON.parse( initDataNode.value ) };
 
-	getInstructors(query, true);
+	getInstructors( query, true );
 	pagination();
 }
 
-const getInstructors = (queryParam, firstLoad = false) => {
+const getInstructors = ( queryParam, firstLoad = false ) => {
 	containerNode.style.opacity = 0.4;
 
-	wp.apiFetch({
-		path: addQueryArgs('lp/v1/instructors', queryParam),
+	wp.apiFetch( {
+		path: addQueryArgs( 'lp/v1/instructors', queryParam ),
 		method: 'GET',
-	}).then((res) => {
-		if (res.data.content !== undefined) {
+	} ).then( ( res ) => {
+		if ( res.data.content !== undefined ) {
 			containerNode.innerHTML = res.data.content;
 		}
 
 		//pagination
-		if (contentAreaNode) {
-			const paginationNode = document.querySelector('.learn-press-pagination');
-			if (paginationNode) {
+		if ( contentAreaNode ) {
+			const paginationNode = document.querySelector( '.learn-press-pagination' );
+			if ( paginationNode ) {
 				paginationNode.remove();
 			}
-			if (res.data.pagination !== undefined) {
-				contentAreaNode.insertAdjacentHTML('beforeend', res.data.pagination);
+			if ( res.data.pagination !== undefined ) {
+				contentAreaNode.insertAdjacentHTML( 'beforeend', res.data.pagination );
 			}
 		}
-	}).catch((err) => {
-		console.log(err);
-	}).finally(() => {
-		if (queryParam.paged) {
-			const optionScroll = {behavior: 'smooth'};
-			instructorNode.scrollIntoView(optionScroll);
+	} ).catch( ( err ) => {
+		console.log( err );
+	} ).finally( () => {
+		if ( queryParam.paged ) {
+			const optionScroll = { behavior: 'smooth' };
+			instructorNode.scrollIntoView( optionScroll );
 		}
 
-		if (firstLoad === false) {
-			const urlPush = addQueryArgs(document.location.origin + document.location.pathname, queryParam);
-			window.history.pushState('', '', urlPush);
+		if ( firstLoad === false ) {
+			const urlPush = addQueryArgs( document.location.origin + '/' + document.location.pathname.split( '/' )?.[ 1 ], queryParam );
+			window.history.pushState( '', '', urlPush );
 		}
 
 		containerNode.style.opacity = 1;
-	});
+	} );
 };
 
 const pagination = () => {
-	document.addEventListener('click', function (event) {
+	document.addEventListener( 'click', function( event ) {
 		const target = event.target;
-		const pagination = target.closest('.learn-press-pagination');
+		const pagination = target.closest( '.learn-press-pagination' );
 
-		if (!pagination) {
+		if ( ! pagination ) {
 			return;
 		}
 
 		let pageLinkNode;
-		if (target.tagName.toLowerCase() === 'a') {
+		if ( target.tagName.toLowerCase() === 'a' ) {
 			pageLinkNode = target;
-		} else if (target.closest('a.page-numbers')) {
-			pageLinkNode = target.closest('a.page-numbers');
+		} else if ( target.closest( 'a.page-numbers' ) ) {
+			pageLinkNode = target.closest( 'a.page-numbers' );
 		} else {
 			return;
 		}
 
 		event.preventDefault();
 
-		const currentPage = parseInt(pagination.querySelector('.current').innerHTML);
+		const currentPage = parseInt( pagination.querySelector( '.current' ).innerHTML );
 		let paged;
 
-		if (pageLinkNode.classList.contains('next')) {
+		if ( pageLinkNode.classList.contains( 'next' ) ) {
 			paged = currentPage + 1;
-		} else if (pageLinkNode.classList.contains('prev')) {
+		} else if ( pageLinkNode.classList.contains( 'prev' ) ) {
 			paged = currentPage - 1;
 		} else {
 			paged = pageLinkNode.innerHTML;
 		}
 
-		query = {...query, paged};
-		getInstructors(query);
-	});
+		query = { ...query, paged };
+		getInstructors( query );
+	} );
 };
