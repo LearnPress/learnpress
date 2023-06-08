8be930ba5aa706beadd3c2202b1a8ed5095abf0c
tungnx <tungnxt89@gmail.com>
2023-06-08 11:24:30 +0700

    = profile = ~ Tweak: add_controls_style_button method, extend from add_controls_style_text.

--- inc/ExternalPlugin/Elementor/LPElementorControls.php
+++ inc/ExternalPlugin/Elementor/LPElementorControls.php
@@ -25,7 +25,7 @@ class LPElementorControls {
 	 *
 	 * @return string[]
 	 */
-	public static function add_start_section( string $id, string $label,
+	private static function add_start_section( string $id, string $label,
 		string $tab = Controls_Manager::TAB_CONTENT, array $args = [] ): array {
 		return [
 			'method' => 'start_controls_section',
@@ -202,84 +202,6 @@ class LPElementorControls {
 	}
 
 	/**
-	 * Group control style for text.
-	 *
-	 * @param string $name_field
-	 * @param string $title
-	 * @param string $el_selector
-	 *
-	 * @return array
-	 */
-	public static function add_control_style_for_el( string $name_field, string $title, string $el_selector ): array {
-		return [
-			"section_style_{$name_field}"     => self::add_start_section(
-				"section_style_{$name_field}",
-				$title,
-				Controls_Manager::TAB_STYLE
-			),
-			"{$name_field}_align"             => [
-				'method' => 'add_responsive_control',
-				'id'     => "{$name_field}_align",
-				[
-					'label'     => esc_html__( 'Alignment', 'learnpress' ),
-					'type'      => Controls_Manager::CHOOSE,
-					'options'   => array(
-						'left'   => array(
-							'title' => esc_html__( 'Left', 'learnpress' ),
-							'icon'  => 'eicon-text-align-left',
-						),
-						'center' => array(
-							'title' => esc_html__( 'Center', 'learnpress' ),
-							'icon'  => 'eicon-text-align-center',
-						),
-						'right'  => array(
-							'title' => esc_html__( 'Right', 'learnpress' ),
-							'icon'  => 'eicon-text-align-right',
-						),
-					),
-					'selectors' => array(
-						'{{WRAPPER}} ' . $el_selector => 'text-align: {{VALUE}}',
-					),
-				],
-			],
-			"{$name_field}_color"             => self::add_control_type_color(
-				"{$name_field}_color",
-				esc_html__( 'Color', 'learnpress' ),
-				[ '{{WRAPPER}} ' . $el_selector => 'color: {{VALUE}}' ]
-			),
-			"{$name_field}_typography"        => self::add_group_control_type(
-				"{$name_field}_typography",
-				Group_Control_Typography::get_type(),
-				"{{WRAPPER}} $el_selector"
-			),
-			"{$name_field}_shadow"            => self::add_group_control_type(
-				"{$name_field}_shadow",
-				Group_Control_Text_Shadow::get_type(),
-				"{{WRAPPER}} $el_selector"
-			),
-			"{$name_field}_spacing"           => [
-				'method' => 'add_responsive_control',
-				"{$name_field}_spacing",
-				array(
-					'label'     => esc_html__( 'Spacing', 'learnpress' ),
-					'type'      => Controls_Manager::SLIDER,
-					'range'     => array(
-						'px' => array(
-							'max' => 100,
-						),
-					),
-					'selectors' => array(
-						'{{WRAPPER}} ' . $el_selector => 'margin-bottom: {{SIZE}}{{UNIT}};',
-					),
-				),
-			],
-			"end_section_style_{$name_field}" => [
-				'method' => 'end_controls_section',
-			],
-		];
-	}
-
-	/**
 	 * Include or exclude more fields.
 	 *
 	 * @param array $fields
@@ -304,90 +226,6 @@ class LPElementorControls {
 		}
 
 		return $fields;
-	}
-
-	/**
-	 * Group control style for button.
-	 *
-	 * @param string $prefix_name
-	 * @param string $selector
-	 * @param array $include
-	 * @param array $exclude
-	 *
-	 * @return array
-	 */
-	public static function add_controls_style_button( string $prefix_name, string $selector,
-		array $include = [], array $exclude = [] ): array {
-
-		$fields = [
-			"{$prefix_name}_btn_margin"           => self::add_responsive_control_type(
-				"{$prefix_name}_btn_margin",
-				esc_html__( 'Margin', 'learnpress' ),
-				[],
-				Controls_Manager::DIMENSIONS,
-				[
-					'size_units' => [ 'px', '%', 'custom' ],
-					'selectors'  => array(
-						"{{WRAPPER}} $selector" => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-					),
-				]
-			),
-			"{$prefix_name}_btn_padding"          => self::add_responsive_control_type(
-				"{$prefix_name}_btn_padding",
-				esc_html__( 'Padding', 'learnpress' ),
-				[],
-				Controls_Manager::DIMENSIONS,
-				[
-					'size_units' => [ 'px', '%', 'custom' ],
-					'selectors'  => array(
-						"{{WRAPPER}} $selector" => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-					),
-				]
-			),
-			"{$prefix_name}_btn_typography"       => self::add_group_control_type(
-				"{$prefix_name}_btn_typography",
-				Group_Control_Typography::get_type(),
-				"{{WRAPPER}} $selector"
-			),
-			"{$prefix_name}_btn_color"            => self::add_control_type_color(
-				"{$prefix_name}_btn_color",
-				esc_html__( 'Text Color', 'learnpress' ),
-				[ "{{WRAPPER}} $selector" => 'color: {{VALUE}}' ]
-			),
-			"{$prefix_name}_btn_color_hover"      => self::add_control_type_color(
-				"{$prefix_name}_btn_color_hover",
-				esc_html__( 'Text Color Hover', 'learnpress' ),
-				[ "{{WRAPPER}} $selector:hover" => 'color: {{VALUE}}' ]
-			),
-			"{$prefix_name}_btn_background"       => self::add_control_type_color(
-				"{$prefix_name}_btn_background",
-				esc_html__( 'Background Color', 'learnpress' ),
-				[ "{{WRAPPER}} $selector" => 'background: {{VALUE}}' ]
-			),
-			"{$prefix_name}_btn_background_hover" => self::add_control_type_color(
-				"{$prefix_name}_btn_background_hover",
-				esc_html__( 'Background Color Hover', 'learnpress' ),
-				[ "{{WRAPPER}} $selector:hover" => 'background: {{VALUE}}' ]
-			),
-			"{$prefix_name}_btn_border"           => self::add_group_control_type(
-				"{$prefix_name}_btn_border",
-				Group_Control_Border::get_type(),
-				"{{WRAPPER}} $selector"
-			),
-			"{$prefix_name}_btn_border_radius"    => self::add_control_type(
-				"{$prefix_name}_btn_border_radius",
-				esc_html__( 'Border Radius', 'learnpress' ),
-				[],
-				Controls_Manager::DIMENSIONS,
-				[
-					'selectors' => [
-						"{{WRAPPER}} $selector" => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-					],
-				]
-			),
-		];
-
-		return self::add_group_style_controls( $fields, $prefix_name, $include, $exclude );
 	}
 
 	/**
@@ -458,6 +296,45 @@ class LPElementorControls {
 				[ "{{WRAPPER}} $selector:hover" => 'background: {{VALUE}}' ]
 			),
 		];
+
+		return self::add_group_style_controls( $fields, $prefix_name, $include, $exclude );
+	}
+
+	/**
+	 * Group control style for button.
+	 *
+	 * @param string $prefix_name
+	 * @param string $selector
+	 * @param array $include
+	 * @param array $exclude
+	 *
+	 * @return array
+	 */
+	public static function add_controls_style_button( string $prefix_name, string $selector,
+		array $include = [], array $exclude = [] ): array {
+
+		$fields = self::add_controls_style_text( $prefix_name, $selector, $include, $exclude );
+		$fields = array_merge(
+			$fields,
+			[
+				"{$prefix_name}_btn_border"        => self::add_group_control_type(
+					"{$prefix_name}_btn_border",
+					Group_Control_Border::get_type(),
+					"{{WRAPPER}} $selector"
+				),
+				"{$prefix_name}_btn_border_radius" => self::add_control_type(
+					"{$prefix_name}_btn_border_radius",
+					esc_html__( 'Border Radius', 'learnpress' ),
+					[],
+					Controls_Manager::DIMENSIONS,
+					[
+						'selectors' => [
+							"{{WRAPPER}} $selector" => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+						],
+					]
+				),
+			]
+		);
 
 		return self::add_group_style_controls( $fields, $prefix_name, $include, $exclude );
 	}
