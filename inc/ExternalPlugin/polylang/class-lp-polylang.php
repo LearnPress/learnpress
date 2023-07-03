0e8449a7a5e088df08b418cefbe1106d2bda3ca9
Khanh Bui <khanhbd@physcode.com>
2023-07-03 10:45:33 +0700

    Fix polylang

--- inc/ExternalPlugin/polylang/class-lp-polylang.php
+++ inc/ExternalPlugin/polylang/class-lp-polylang.php
@@ -30,6 +30,7 @@ class LP_Polylang {
 		add_filter( 'lp/course/query/filter', [ $this, 'filter_query_courses' ], 10 );
 		add_filter( 'lp/user/course/query/filter', [ $this, 'filter_query_user_courses' ], 10 );
 		add_filter( 'pll_the_language_link', [ $this, 'get_link_switcher' ], 10, 3 );
+		//Rewrite URL when force lang = The language is set from the directory name in pretty permalinks
 		add_filter( 'learn-press/rewrite/rules', [ $this, 'pll_rewrite_url_for_lp' ] );
 	}
 
@@ -199,28 +200,108 @@ class LP_Polylang {
 
 		return $url;
 	}
-
+	/**
+	 * [pll_rewrite_url_for_lp rewrite url when  The language is set from the directory name in pretty permalinks Polylang]
+	 * @param  [array] $rules [rewrite url rule]
+	 * @return [array]        [rewrite url rule]
+	 */
 	public function pll_rewrite_url_for_lp( $rules ) {
 		if ( ! is_callable( 'pll_default_language' ) || ! is_callable( 'pll_current_language' ) ) {
-			return;
+			return $rules;
 		}
 		$lang_default = pll_default_language();
 		$lang_current = pll_current_language();
-		if ( $lang_current != $lang_default ) {
-			$pll_option = $this->get_pll_options();
-			// if ( $rules )
+		if ( $lang_current == $lang_default ) {
+			return $rules;
+		}
+		$pll_options = $this->get_pll_options();
+		if ( ! $pll_options ) {
+			return $rules;
+		}
+		if ( $pll_options['force_lang'] == 1 ) {
+			$course_slug = LP_Settings::get_option( 'course_base', 'courses' );
+			if ( empty( $course_slug ) ) {
+				$course_slug = 'courses';
+			}
+			$course_slug       = preg_replace( '!^/!', '', $course_slug );
+			$lesson_slug       = urldecode( sanitize_title_with_dashes( LP_Settings::get_option( 'lesson_slug', 'lessons' ) ) );
+			$quiz_slug         = urldecode( sanitize_title_with_dashes( LP_Settings::get_option( 'quiz_slug', 'quizzes' ) ) );
+			$course_item_slugs = array(
+				LP_LESSON_CPT => $lesson_slug,
+				LP_QUIZ_CPT   => $quiz_slug,
+			);
+			$lang_slug         = $pll_options['rewrite'] == 1 ? $lang_current : 'language/' . $lang_current;
+			if ( isset( $rules['course-items'] ) && ! empty( $rules['course-items'] )
+				&& isset( $pll_options['post_types'] ) && ! empty( $pll_options['post_types'] ) ) {
+				foreach ( $rules['course-items'] as $post_type => $rule ) {
+					if ( in_array( $post_type, $pll_options['post_types'] ) ) {
+						$rules['course-items'][ $post_type ] = array(
+							"^{$lang_slug}/{$course_slug}/([^/]+)(?:/{$course_item_slugs[ $post_type ]}/([^/]+))?/?$" =>
+							'index.php?' . LP_COURSE_CPT . '=$matches[1]&course-item=$matches[2]&item-type=' . $post_type . '&lang=' . $lang_current,
+						);
+					}
+				}
+				if ( class_exists( 'LP_Addon_Assignment_Preload' ) ) {
+					$assignment_slug                            = urldecode( sanitize_title_with_dashes( LP_Settings::get_option( 'assignment_slug', 'assignments' ) ) );
+					$rules['course-items'][ LP_ASSIGNMENT_CPT ] = [
+						"^{$lang_slug}/{$course_slug}/([^/]+)(?:/{$assignment_slug}/([^/]+))?/?$" =>
+						'index.php?' . LP_COURSE_CPT . '=$matches[1]&course-item=$matches[2]&item-type=' . LP_ASSIGNMENT_CPT . '&lang=' . $lang_current,
+					];
+				}
+				if ( class_exists( 'LP_Addon_H5p_Preload' ) ) {
+					$h5p_slug                            = urldecode( sanitize_title_with_dashes( LP_Settings::get_option( 'h5p_slug', 'h5p' ) ) );
+					$rules['course-items'][ LP_H5P_CPT ] = [
+						"^{$lang_slug}/{$course_slug}/([^/]+)(?:/{$h5p_slug}/([^/]+))?/?$" =>
+						'index.php?' . LP_COURSE_CPT . '=$matches[1]&course-item=$matches[2]&item-type=' . LP_H5P_CPT . '&lang=' . $lang_current,
+					];
+				}
+			}
+			if ( isset( $rules['course-with-cat-items'] ) && ! empty( $rules['course-with-cat-items'] )
+				&& isset( $pll_options['post_types'] ) && ! empty( $pll_options['post_types'] ) ) {
+				foreach ( $rules['course-with-cat-items'] as $post_type => $rule ) {
+					if ( in_array( $post_type, $pll_options['post_types'] ) ) {
+						$rules['course-with-cat-items'][ $post_type ] = [
+							"^{$lang_slug}/{$course_slug}(?:/{$course_item_slug}/([^/]+))?/?$" =>
+								'index.php?' . LP_COURSE_CPT . '=$matches[2]&course_category=$matches[1]&course-item=$matches[3]&item-type=' . $post_type . '&lang=' . $lang_current,
+						];
+					}
+				}
+				if ( class_exists( 'LP_Addon_Assignment_Preload' ) ) {
+					$assignment_slug                                     = urldecode(
+						sanitize_title_with_dashes(
+							LP_Settings::get_option( 'assignment_slug', 'assignments' )
+						)
+					);
+					$rules['course-with-cat-items'][ LP_ASSIGNMENT_CPT ] = [
+						"^{$lang_slug}/{$course_slug}(?:/{$assignment_slug}/([^/]+))?/?$" =>
+							'index.php?' . LP_COURSE_CPT . '=$matches[2]&course_category=$matches[1]&course-item=$matches[3]&item-type=' . LP_ASSIGNMENT_CPT . '&lang=' . $lang_current,
+					];
+				}
+				if ( class_exists( 'LP_Addon_H5p_Preload' ) ) {
+					$h5p_slug                                     = urldecode( sanitize_title_with_dashes( LP_Settings::get_option( 'h5p_slug', 'h5p' ) ) );
+					$rules['course-with-cat-items'][ LP_H5P_CPT ] = [
+						"^{$lang_slug}/{$course_slug}(?:/{$h5p_slug}/([^/]+))?/?$" =>
+							'index.php?' . LP_COURSE_CPT . '=$matches[2]&course_category=$matches[1]&course-item=$matches[3]&item-type=' . LP_H5P_CPT . '&lang=' . $lang_current,
+					];
+				}
+			}
 		}
 		return $rules;
 	}
 
+
+	/**
+	 * [get_pll_options get polylang settings]
+	 * @return [array]
+	 */
 	public function get_pll_options() {
 		global $wpdb;
-		$result = $wpdb->get_var( 
-			$wpdb->prepare( 
+		$result = $wpdb->get_var(
+			$wpdb->prepare(
 				"SELECT option_value FROM $wpdb->options WHERE option_name = %s",
 				'polylang'
-			 )
-		 );
+			)
+		);
 		if ( $wpdb->last_error ) {
 			throw new Exception( $wpdb->last_error );
 		}
