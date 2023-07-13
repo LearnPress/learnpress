af9996674a1230b9c2fdfb9afcf038c2938a2d10
quyennv1 <quyennv@ohio-digital.com>
2023-07-13 15:29:51 +0700

    update LP style except learning page

--- templates/single-course/content-item/popup-header.php
+++ templates/single-course/content-item/popup-header.php
@@ -19,35 +19,32 @@ if ( ! isset( $course ) || ! isset( $user ) || ! isset( $percentage ) ||
 
 <div id="popup-header">
     <div class="popup-header__inner">
-        <div class="course-header-progress">
-            <h2 class="course-title">
-                <a
-                    href="<?php echo esc_url_raw( $course->get_permalink() ); ?>"><?php echo wp_kses_post( $course->get_title() ); ?></a>
-            </h2>
+        <h2 class="course-title">
+            <a
+                href="<?php echo esc_url_raw( $course->get_permalink() ); ?>"><?php echo wp_kses_post( $course->get_title() ); ?></a>
+        </h2>
 
-            <?php if ( $user->has_enrolled_or_finished( $course->get_id() ) ) : ?>
-            <div class="items-progress" data-total-items="<?php echo esc_attr( $total_items ); ?>">
-                <span class="number">
-                    <?php
+        <?php if ( $user->has_enrolled_or_finished( $course->get_id() ) ) : ?>
+        <div class="items-progress" data-total-items="<?php echo esc_attr( $total_items ); ?>">
+            <span class="number">
+                <?php
 					echo
 						wp_sprintf(
 							__(
-								'<span class="items-completed">%1$s</span> / %2$d items',
+								'<span class="items-completed">%1$s</span> of %2$d items',
 								'learnpress'
 							),
 							esc_html( $completed_items ),
 							esc_html( $course->count_items() )
 						);
 					?>
-                </span>
-                <div class="learn-press-progress">
-                    <div class="learn-press-progress__active" data-value="<?php echo esc_attr( $percentage ); ?>%;">
-                    </div>
+            </span>
+            <div class="learn-press-progress">
+                <div class="learn-press-progress__active" data-value="<?php echo esc_attr( $percentage ); ?>%;">
                 </div>
             </div>
-            <?php endif; ?>
         </div>
-
+        <?php endif; ?>
     </div>
     <a href="<?php echo esc_url_raw( $course->get_permalink() ); ?>" class="back-course"><i class="fa fa-times"></i></a>
 </div>
