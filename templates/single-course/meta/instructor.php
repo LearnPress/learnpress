1701735cb24c1f321e95c78b2c61758b9727570d
tungnx <tungnxt89@gmail.com>
2023-07-19 08:56:01 +0700

    = 4.2.3.2 = ~ Change link instructor on single course

--- templates/single-course/meta/instructor.php
+++ templates/single-course/meta/instructor.php
@@ -2,10 +2,12 @@
 /**
  * Template for displaying course instructor in primary-meta section.
  *
- * @version 4.0.0
+ * @version 4.0.1
  * @author  ThimPress
  * @package LearnPress/Templates
  */
+
+use LearnPress\TemplateHooks\Instructor\SingleInstructorTemplate;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -13,14 +15,29 @@ $course = learn_press_get_course();
 if ( ! $course ) {
 	return;
 }
+
+$instructor = $course->get_instructor();
+if ( ! $instructor ) {
+	return;
+}
 ?>
 
 <div class="meta-item meta-item-instructor">
 	<div class="meta-item__image">
-		<?php echo wp_kses_post( $course->get_instructor()->get_profile_picture() ); ?>
+		<?php echo wp_kses_post( $instructor->get_profile_picture() ); ?>
 	</div>
 	<div class="meta-item__value">
 		<label><?php esc_html_e( 'Instructor', 'learnpress' ); ?></label>
-		<div><?php echo wp_kses_post( $course->get_instructor_html() ); ?></div>
+		<div>
+			<?php
+			echo wp_kses_post(
+				sprintf(
+					'<a href="%s">%s</a>',
+					$instructor->get_url_instructor(),
+					SingleInstructorTemplate::instance()->html_display_name( $instructor )
+				)
+			);
+			?>
+		</div>
 	</div>
 </div>
