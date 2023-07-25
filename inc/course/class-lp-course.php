95eed5a361401ebbf9f2f122785c7c81853e70f3
Tungnx <tungnxt89@gmail.com>
2023-07-25 16:04:46 +0700

    = 4.2.3.3 = ~ Added: handle_params_for_query_courses method.

--- inc/course/class-lp-course.php
+++ inc/course/class-lp-course.php
@@ -482,6 +482,91 @@ if ( ! class_exists( 'LP_Course' ) ) {
 		}
 
 		/**
+		 * Handle params before query courses
+		 *
+		 * @param array $param
+		 * @param LP_Course_Filter $filter
+		 * @since 4.2.3.3
+		 * @return void
+		 */
+		public static function handle_params_for_query_courses( LP_Course_Filter &$filter, array $param = [] ) {
+			$filter->page       = absint( $param['paged'] ?? 1 );
+			$filter->post_title = LP_Helper::sanitize_params_submitted( trim( $param['c_search'] ?? '' ) );
+
+			// Get Columns
+			$fields_str = LP_Helper::sanitize_params_submitted( urldecode( $param['c_fields'] ?? '' ) );
+			if ( ! empty( $fields_str ) ) {
+				$fields         = explode( ',', $fields_str );
+				$filter->fields = $fields;
+			}
+
+			// Exclude Columns
+			$fields_exclude_str = LP_Helper::sanitize_params_submitted( urldecode( $param['c_exclude_fields'] ?? '' ) );
+			if ( ! empty( $fields_exclude_str ) ) {
+				$fields_exclude         = explode( ',', $fields_exclude_str );
+				$filter->exclude_fields = $fields_exclude;
+			}
+
+			// Author
+			$filter->post_author = LP_Helper::sanitize_params_submitted( $param['c_author'] ?? 0 );
+			$author_ids_str      = LP_Helper::sanitize_params_submitted( $param['c_authors'] ?? 0 );
+			if ( ! empty( $author_ids_str ) ) {
+				$author_ids           = explode( ',', $author_ids_str );
+				$filter->post_authors = $author_ids;
+			}
+
+			/**
+			 * Sort by
+			 * 1. on_sale
+			 * 2. on_free
+			 * 3. on_paid
+			 * 4. on_feature
+			 */
+			if ( ! empty( $param['sort_by'] ) ) {
+				$filter->sort_by[] = $param['sort_by'];
+			}
+
+			// Sort by level
+			$levels_str = LP_Helper::sanitize_params_submitted( urldecode( $param['c_level'] ?? '' ) );
+			if ( ! empty( $levels_str ) ) {
+				$levels_str     = str_replace( 'all', '', $levels_str );
+				$levels         = explode( ',', $levels_str );
+				$filter->levels = $levels;
+			}
+
+			// Find by category
+			$term_ids_str = LP_Helper::sanitize_params_submitted( urldecode( $param['term_id'] ?? '' ) );
+			if ( ! empty( $term_ids_str ) ) {
+				$term_ids         = explode( ',', $term_ids_str );
+				$filter->term_ids = $term_ids;
+			}
+
+			// Find by tag
+			$tag_ids_str = LP_Helper::sanitize_params_submitted( urldecode( $param['tag_id'] ?? '' ) );
+			if ( ! empty( $tag_ids_str ) ) {
+				$tag_ids         = explode( ',', $tag_ids_str );
+				$filter->tag_ids = $tag_ids;
+			}
+
+			// Order by
+			$filter->order_by = LP_Helper::sanitize_params_submitted( ! empty( $param['order_by'] ) ? $param['order_by'] : 'post_date' );
+			$filter->order    = LP_Helper::sanitize_params_submitted( ! empty( $param['order'] ) ? $param['order'] : 'DESC' );
+			$filter->limit    = $param['limit'] ?? LP_Settings::get_option( 'archive_course_limit', 10 );
+
+			// For search suggest courses
+			if ( ! empty( $param['c_suggest'] ) ) {
+				$filter->only_fields = [ 'ID', 'post_title' ];
+				$filter->limit       = apply_filters( 'learn-press/rest-api/courses/suggest-limit', 10 );
+				$filter->max_limit   = apply_filters( 'learn-press/rest-api/courses/suggest-max-limit', 10 );
+			}
+
+			$return_type = $param['return_type'] ?? 'html';
+			if ( 'json' !== $return_type ) {
+				$filter->only_fields = array( 'DISTINCT(ID) AS ID' );
+			}
+		}
+
+		/**
 		 * Get list course
 		 * Order By: price, title, rating, date ...
 		 * Order: ASC, DES
