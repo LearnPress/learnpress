c6a68e3aab100a023a965c450df9977788d45e33
truongpn <truongpn@aht-asia.com>
2023-08-18 17:14:13 +0700

    add widgets single instructor

--- inc/ExternalPlugin/Elementor/lp-elementor-widgets-config.php
+++ inc/ExternalPlugin/Elementor/lp-elementor-widgets-config.php
@@ -10,6 +10,9 @@ use LearnPress\ExternalPlugin\Elementor\Widgets\CourseListElementor;
 use LearnPress\ExternalPlugin\Elementor\Widgets\Instructor\Sections\InstructorButtonViewElementor;
 use LearnPress\ExternalPlugin\Elementor\Widgets\Instructor\Sections\InstructorDescriptionElementor;
 use LearnPress\ExternalPlugin\Elementor\Widgets\Instructor\Sections\InstructorTitleElementor;
+use LearnPress\ExternalPlugin\Elementor\Widgets\Instructor\Sections\InstructorAvatarElementor;
+use LearnPress\ExternalPlugin\Elementor\Widgets\Instructor\Sections\InstructorCountStudentsElementor;
+use LearnPress\ExternalPlugin\Elementor\Widgets\Instructor\Sections\InstructorCountCoursesElementor;
 use LearnPress\ExternalPlugin\Elementor\Widgets\Instructor\ListInstructorsElementor;
 use LearnPress\ExternalPlugin\Elementor\Widgets\LoginUserFormElementor;
 use LearnPress\ExternalPlugin\Elementor\Widgets\RegisterUserFormElementor;
@@ -19,15 +22,18 @@ return apply_filters(
 	'lp/elementor/widgets',
 	[
 		//'single-instructor'      => SingleInstructorElementor::class,
-		'list-instructors'       => ListInstructorsElementor::class,
-		'instructor-title'       => InstructorTitleElementor::class,
-		'instructor-description' => InstructorDescriptionElementor::class,
-		'instructor-button-view' => InstructorButtonViewElementor::class,
-		'become-a-teacher'       => BecomeATeacherElementor::class,
-		'login-form'             => LoginUserFormElementor::class,
-		'register-form'          => RegisterUserFormElementor::class,
-		'list-courses'           => CourseListElementor::class,
-		'list-courses-by-page'   => ListCoursesByPageElementor::class,
+		'list-instructors'       	=> ListInstructorsElementor::class,
+		'instructor-title'       	=> InstructorTitleElementor::class,
+		'instructor-description' 	=> InstructorDescriptionElementor::class,
+		'instructor-button-view' 	=> InstructorButtonViewElementor::class,
+		'instructor-avatar'		 	=> InstructorAvatarElementor::class,
+		'instructor-count-students'	=> InstructorCountStudentsElementor::class,
+		'instructor-count-courses'	=> InstructorCountCoursesElementor::class,
+		'become-a-teacher'       	=> BecomeATeacherElementor::class,
+		'login-form'             	=> LoginUserFormElementor::class,
+		'register-form'          	=> RegisterUserFormElementor::class,
+		'list-courses'           	=> CourseListElementor::class,
+		'list-courses-by-page'   	=> ListCoursesByPageElementor::class,
 		// 'course-material'        => CourseMaterialElementor::class,
 	]
 );
