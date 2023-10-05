fc2409c330f19f9bbb3260ca4666e52dd646e02c
Tungnx <tungnxt89@gmail.com>
2023-10-05 08:40:45 +0700

    = assign-course = ~ Rename path folder models to Models

--- learnpress.php
+++ learnpress.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://thimpress.com/learnpress
  * Description: LearnPress is a WordPress complete solution for creating a Learning Management System (LMS). It can help you to create courses, lessons and quizzes.
  * Author: ThimPress
- * Version: 4.2.4
+ * Version: 4.2.5-beta-1
  * Author URI: http://thimpress.com
  * Requires at least: 5.8
  * Tested up to: 6.3.1
@@ -292,10 +292,10 @@ if ( ! class_exists( 'LearnPress' ) ) {
 			CourseMaterialTemplate::instance();
 
 			// Models
-			require_once 'inc/models/class-lp-rest-response.php';
-			include_once 'inc/models/steps/class-lp-group-step.php';
-			include_once 'inc/models/steps/class-lp-step.php';
-			require_once 'inc/models/class-lp-course-extra-info-fast-query-model.php';
+			require_once 'inc/Models/class-lp-rest-response.php';
+			include_once 'inc/Models/steps/class-lp-group-step.php';
+			include_once 'inc/Models/steps/class-lp-step.php';
+			require_once 'inc/Models/class-lp-course-extra-info-fast-query-model.php';
 
 			// Handle steps.
 			require_once 'inc/handle-steps/class-lp-handle-steps.php';
