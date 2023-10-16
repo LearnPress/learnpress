e1689d33f3dfc1603bffc9ab870163341ae333bb
Khanh Bui <khanhbd@physcode.com>
2023-10-16 17:48:36 +0700

    update chart

--- assets/src/apps/js/admin/pages/statistics-beta.js
+++ assets/src/apps/js/admin/pages/statistics-beta.js
@@ -1,5 +1,5 @@
 // console.log('load-js');
-var test_response;
+
 document.addEventListener("DOMContentLoaded", function() {
 	const orderStatisticsLoad = ( ) => {
 		const elementLoad = document.querySelector( 'input.statistics-type' );
@@ -20,9 +20,29 @@ document.addEventListener("DOMContentLoaded", function() {
 			if ( status === 'error' ) {
 				throw new Error( message || 'Error' );
 			}
+			generateOrderChart( data.chart_data );
 		} ).catch((err) => {
 			console.log(err); 
 		}).finally(() => {});
+	}
+	const generateOrderChart = ( data = [] ) => {
+		let canvas = document.getElementById( 'orders-chart-content' );
+		
+			// const labels = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
+		const chart_data = {
+		  labels: data.labels,
+		  datasets: [
+		    {
+		      label: 'Completed Orders',
+		      borderColor: 'blue',
+		      data:data.data,
+		      backgroundColor: 'blue',
+		    },
+		  ]
+		};
+		const config = { type: 'line', data: chart_data, options: { responsive: true, plugins: { title: { display: true, text: 'Completed Orders' } }, scales: { y: { min: 0, ticks: {stepSize: 1 } } } }, };
+
+		const chart = new Chart( canvas, config );
 	}
 	orderStatisticsLoad();
 });
--- inc/rest-api/v1/admin/class-lp-admin-rest-statistics-controller.php
+++ inc/rest-api/v1/admin/class-lp-admin-rest-statistics-controller.php
@@ -53,13 +53,15 @@ class LP_REST_Admin_Statistics_Controller extends LP_Abstract_REST_Controller {
 			if ( !$params['filterType'] ) {
 				$filter_type = 'date';
 				$time        = current_time('Y-m-d');
+				$time = '2023/09/15';
 			}
 			$lp_order_db = LP_Order_DB::getInstance();
 			$statistics = $lp_order_db->get_order_statics( $filter_type, $time );
-			$completed_data = $lp_order_db->get_completed_order_data( $filter_type, $time );
+			$completed_orders = $lp_order_db->get_completed_order_data( $filter_type, $time );
+			$chart_data = $this->process_date_order_complete_data( $completed_orders );
 			$data = array(
 				'statistics' => $statistics,
-				'chart'      => $completed_data,
+				'chart_data' => $chart_data,
 			);
 			$response->data = $data;
 			$response->status  = 'success';
@@ -69,8 +71,28 @@ class LP_REST_Admin_Statistics_Controller extends LP_Abstract_REST_Controller {
 		}
 		return rest_ensure_response( $response );
 	}
+	public function process_date_order_complete_data( array $input_data ) {
+		$data = [];
+		for( $i=0; $i<24;$i++ ){
+		    $row = new stdClass();
+		    $row->order_time = $i; 
+		    $row->count_order = 0;
+		    $data[ $i ] = $row;
+		}
+		if( ! empty( $input_data ) ) {
+			foreach ( $input_data as $row ) {
+			    $data[ $row->order_time ] = $row;
+			}
+		}
+		$chart_data = array();
+		foreach ( $data as $row ) {
+			$chart_data['labels'][] = (int)$row->order_time;
+			$chart_data['data'][]   = (int)$row->count_order;
+		}
+		return $chart_data;
+	}
 	public function permission_check( $request ) {
-		return apply_filters( 'learnpress/admin-statistics/rest-permission', current_user_can( 'administrator' ) );
+		return apply_filters( 'learnpress/admin-statistics/permission', current_user_can( 'administrator' ) );
 	}
 	public static function getInstance() {
 		if ( is_null( self::$_instance ) ) {
