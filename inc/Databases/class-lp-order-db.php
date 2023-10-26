70c456e7cdf675101fd58cba563112fa7a4e0d12
Khanh Bui <khanhbd@physcode.com>
2023-10-26 17:40:34 +0700

    edit time field

--- inc/Databases/class-lp-order-db.php
+++ inc/Databases/class-lp-order-db.php
@@ -163,36 +163,36 @@ class LP_Order_DB extends LP_Database {
 		);
 	}
 
-	public function chart_filter_date_group_by( LP_Filter $filter ) {
-		$filter->only_fields[] = 'HOUR(p.post_date) as order_time';
+	public function chart_filter_date_group_by( LP_Filter $filter, string $time_field ) {
+		$filter->only_fields[] = "HOUR($time_field) as order_time";
 		$filter->group_by      = 'order_time';
 		return $filter;
 	}
 
-	public function chart_filter_previous_days_group_by( LP_Filter $filter ) {
-		$filter->only_fields[] = 'CAST(p.post_date AS DATE) as order_time';
+	public function chart_filter_previous_days_group_by( LP_Filter $filter, string $time_field ) {
+		$filter->only_fields[] = "CAST($time_field AS DATE) as order_time";
 		$filter->group_by      = 'order_time';
 		return $filter;
 	}
 
-	public function chart_filter_month_group_by( LP_Filter $filter ) {
-		$filter->only_fields[] = 'DAY(p.post_date) as order_time';
+	public function chart_filter_month_group_by( LP_Filter $filter, string $time_field ) {
+		$filter->only_fields[] = "DAY($time_field) as order_time";
 		$filter->group_by      = 'order_time';
 		return $filter;
 	}
 
-	public function chart_filter_previous_months_group_by( LP_Filter $filter ) {
-		$filter->only_fields[] = 'DATE_FORMAT( p.post_date , "%m-%Y") as order_time';
+	public function chart_filter_previous_months_group_by( LP_Filter $filter, string $time_field ) {
+		$filter->only_fields[] = "DATE_FORMAT( $time_field , '%m-%Y') as order_time";
 		$filter->group_by      = 'order_time';
 		return $filter;
 	}
 
-	public function chart_filter_year_group_by( LP_Filter $filter ) {
-		$filter->only_fields[] = 'MONTH(p.post_date) as order_time';
+	public function chart_filter_year_group_by( LP_Filter $filter, string $time_field ) {
+		$filter->only_fields[] = "MONTH($time_field) as order_time";
 		$filter->group_by      = 'order_time';
 		return $filter;
 	}
-	public function chart_filter_custom_group_by( LP_Filter $filter, $dates ) {
+	public function chart_filter_custom_group_by( LP_Filter $filter, array $dates, string $time_field ) {
 		$diff1 = date_create( $dates[0] );
 		$diff2 = date_create( $dates[1] );
 		if ( ! $diff1 || ! $diff2 ) {
@@ -205,63 +205,63 @@ class LP_Order_DB extends LP_Database {
 		if ( $y < 1 ) {
 			if ( $m <= 1 ) {
 				if ( $d < 1 ) {
-					$filter = $this->chart_filter_date_group_by( $filter );
+					$filter = $this->chart_filter_date_group_by( $filter, $time_field );
 				} else {
-					$filter = $this->chart_filter_previous_days_group_by( $filter );
+					$filter = $this->chart_filter_previous_days_group_by( $filter, $time_field );
 				}
 			} else {
-				$filter = $this->chart_filter_previous_months_group_by( $filter );
+				$filter = $this->chart_filter_previous_months_group_by( $filter, $time_field );
 			}
 		} elseif ( $y < 2 ) {
-			$filter = $this->chart_filter_previous_months_group_by( $filter );
+			$filter = $this->chart_filter_previous_months_group_by( $filter, $time_field );
 		} elseif ( $y < 5 ) {
-			$filter->only_fields[] = $this->wpdb->prepare( 'CONCAT( %s, QUARTER(p.post_date) ,%s, Year(p.post_date)) as order_time', [ 'q', '-' ] );
+			$filter->only_fields[] = $this->wpdb->prepare( "CONCAT( %s, QUARTER($time_field) ,%s, Year($time_field)) as order_time", [ 'q', '-' ] );
 			$filter->group_by = 'order_time';
 		} else {
-			$filter->only_fields[] = 'YEAR(p.post_date) as order_time';
+			$filter->only_fields[] = "YEAR($time_field) as order_time";
 			$filter->group_by      = 'order_time';
 		}
 		return $filter;
 	}
 
-	public function date_filter( LP_Filter $filter, string $date ) {
-		$filter->where[] = $this->wpdb->prepare( 'AND cast( p.post_date as DATE)= cast(%s as DATE)', $date );
+	public function date_filter( LP_Filter $filter, string $date, string $time_field ) {
+		$filter->where[] = $this->wpdb->prepare( "AND cast( $time_field as DATE)= cast(%s as DATE)", $date );
 		return $filter;
 	}
 
-	public function previous_days_filter( LP_Filter $filter, int $value ) {
+	public function previous_days_filter( LP_Filter $filter, int $value, string $time_field ) {
 		if ( $value < 2 ) {
 			throw new Exception( 'Day must be greater than 2 days.', 'learnpress' );
 		}
-		$filter->where[] = $this->wpdb->prepare( 'AND p.post_date >= DATE_ADD(CURDATE(), INTERVAL -%d DAY)', $value );
+		$filter->where[] = $this->wpdb->prepare( "AND $time_field >= DATE_ADD(CURDATE(), INTERVAL -%d DAY)", $value );
 		return $filter;
 	}
 
-	public function month_filter( LP_Filter $filter, string $date ) {
-		$filter->where[] = $this->wpdb->prepare( 'AND EXTRACT(YEAR_MONTH FROM p.post_date)= EXTRACT(YEAR_MONTH FROM %s)', $date );
+	public function month_filter( LP_Filter $filter, string $date, string $time_field ) {
+		$filter->where[] = $this->wpdb->prepare( "AND EXTRACT(YEAR_MONTH FROM $time_field)= EXTRACT(YEAR_MONTH FROM %s)", $date );
 		return $filter;
 	}
 
-	public function previous_months_filter( LP_Filter $filter, int $value ) {
+	public function previous_months_filter( LP_Filter $filter, int $value, string $time_field ) {
 		if ( $value < 2 ) {
 			throw new Exception( 'Values must be greater than 2 months.', 'learnpress' );
 		}
-		$filter->where[] = $this->wpdb->prepare( 'AND EXTRACT(YEAR_MONTH FROM p.post_date) >= EXTRACT(YEAR_MONTH FROM DATE_ADD(CURDATE(), INTERVAL -%d MONTH))', $value );
+		$filter->where[] = $this->wpdb->prepare( "AND EXTRACT(YEAR_MONTH FROM $time_field) >= EXTRACT(YEAR_MONTH FROM DATE_ADD(CURDATE(), INTERVAL -%d MONTH))", $value );
 		return $filter;
 	}
 
-	public function year_filter( LP_Filter $filter, string $date ) {
-		$filter->where[] = $this->wpdb->prepare( 'AND YEAR(p.post_date)= YEAR(%s)', $date );
+	public function year_filter( LP_Filter $filter, string $date, string $time_field ) {
+		$filter->where[] = $this->wpdb->prepare( "AND YEAR($time_field)= YEAR(%s)", $date );
 		return $filter;
 	}
 
-	public function custom_time_filter( LP_Filter $filter, array $dates ) {
+	public function custom_time_filter( LP_Filter $filter, array $dates, string $time_field ) {
 		if ( empty( $dates ) ) {
 			throw new Exception( 'Select date', 'learnpress' );
 		}
 		sort( $dates );
 		$filter->where[] = $this->wpdb->prepare(
-			'AND (DATE(p.post_date) BETWEEN %s AND %s)',
+			"AND (DATE($time_field) BETWEEN %s AND %s)",
 			date( 'Y-m-d', strtotime( $dates[0] ) ),
 			date( 'Y-m-d', strtotime( $dates[1] ) )
 		);
@@ -274,6 +274,7 @@ class LP_Order_DB extends LP_Database {
 		$oi_table                 = $this->tb_lp_order_items;
 		$oim_table                = $this->tb_lp_order_itemmeta;
 		$filter->only_fields[]    = 'SUM(oim.meta_value) as net_sales';
+		$time_field               = 'p.post_date';
 		$filter->join             = [
 			"INNER JOIN $oi_table AS oi ON p.ID = oi.order_id",
 			"INNER JOIN $oim_table AS oim ON oi.order_item_id = oim.learnpress_order_item_id",
@@ -284,9 +285,9 @@ class LP_Order_DB extends LP_Database {
 			$this->wpdb->prepare( 'AND p.post_status=%s', LP_ORDER_COMPLETED_DB ),
 			$this->wpdb->prepare( 'AND oim.meta_key=%s', '_total' ),
 		];
-		$filter                   = $this->filter_time( $filter, $type, $value );
-		$filter                   = $this->chart_filter_group_by( $filter, $type, $value );
-		$filter->order_by         = 'p.post_date';
+		$filter                   = $this->filter_time( $filter, $type, $time_field, $value );
+		$filter                   = $this->chart_filter_group_by( $filter, $type, $time_field, $value );
+		$filter->order_by         = $time_field;
 		$filter->order            = 'asc';
 		$filter->run_query_count  = false;
 		$result                   = $this->execute( $filter );
@@ -305,16 +306,17 @@ class LP_Order_DB extends LP_Database {
 		$filter                   = new LP_Order_Filter();
 		$filter->collection       = $this->tb_posts;
 		$filter->collection_alias = 'p';
+		$time_field               = 'p.post_date';
 
 		// count completed orders
 		$filter->only_fields[] = 'count( p.ID) as x_axis_data';
-		$filter                = $this->filter_time( $filter, $type, $value );
-		$filter                = $this->chart_filter_group_by( $filter, $type, $value );
+		$filter                = $this->filter_time( $filter, $type, $time_field, $value );
+		$filter                = $this->chart_filter_group_by( $filter, $type, $time_field, $value );
 
 		$filter->where[] = $this->wpdb->prepare( 'AND p.post_status=%s', LP_ORDER_COMPLETED_DB );
 		$filter->limit   = -1;
 
-		$filter->order_by = 'p.post_date';
+		$filter->order_by = $time_field;
 		$filter->order    = 'asc';
 
 		$filter->run_query_count = false;
@@ -322,54 +324,54 @@ class LP_Order_DB extends LP_Database {
 		$result = $this->execute( $filter );
 		return $result;
 	}
-	public function filter_time( LP_Filter $filter, string $type, $value = false ) {
+	public function filter_time( LP_Filter $filter, string $type, string $time_field, $value = false ) {
 		if ( ! $value ) {
 			throw new Exception( 'Empty statistic time', 'learnpress' );
 		}
 		switch ( $type ) {
 			case 'date':
-				$filter = $this->date_filter( $filter, $value );
+				$filter = $this->date_filter( $filter, $value, $time_field );
 				break;
 			case 'month':
-				$filter = $this->month_filter( $filter, $value );
+				$filter = $this->month_filter( $filter, $value, $time_field );
 				break;
 			case 'year':
-				$filter = $this->year_filter( $filter, $value );
+				$filter = $this->year_filter( $filter, $value, $time_field );
 				break;
 			case 'previous_days':
-				$filter = $this->previous_days_filter( $filter, (int) $value );
+				$filter = $this->previous_days_filter( $filter, (int) $value, $time_field );
 				break;
 			case 'previous_months':
-				$filter = $this->previous_months_filter( $filter, (int) $value );
+				$filter = $this->previous_months_filter( $filter, (int) $value, $time_field );
 				break;
 			case 'custom':
 				$value = explode( '+', $value );
 				if ( count( $value ) !== 2 ) {
 					throw new Exception( 'Invalid custom time', 'learnpress' );
 				}
-				$filter = $this->custom_time_filter( $filter, $value );
+				$filter = $this->custom_time_filter( $filter, $value, $time_field );
 			default:
 				// code...
 				break;
 		}
 		return $filter;
 	}
-	public function chart_filter_group_by( LP_Filter $filter, string $type, $value = false ) {
+	public function chart_filter_group_by( LP_Filter $filter, string $type, string $time_field , $value = false ) {
 		switch ( $type ) {
 			case 'date':
-				$filter = $this->chart_filter_date_group_by( $filter );
+				$filter = $this->chart_filter_date_group_by( $filter, $time_field );
 				break;
 			case 'month':
-				$filter = $this->chart_filter_month_group_by( $filter );
+				$filter = $this->chart_filter_month_group_by( $filter, $time_field );
 				break;
 			case 'year':
-				$filter = $this->chart_filter_year_group_by( $filter );
+				$filter = $this->chart_filter_year_group_by( $filter, $time_field );
 				break;
 			case 'previous_days':
-				$filter = $this->chart_filter_previous_days_group_by( $filter );
+				$filter = $this->chart_filter_previous_days_group_by( $filter, $time_field );
 				break;
 			case 'previous_months':
-				$filter = $this->chart_filter_previous_months_group_by( $filter );
+				$filter = $this->chart_filter_previous_months_group_by( $filter, $time_field );
 				break;
 			case 'custom':
 				if ( empty( $value ) ) {
@@ -379,7 +381,7 @@ class LP_Order_DB extends LP_Database {
 				if ( count( $value ) !== 2 ) {
 					throw new Exception( 'Invalid custom time', 'learnpress' );
 				}
-				$filter = $this->chart_filter_custom_group_by( $filter, $value );
+				$filter = $this->chart_filter_custom_group_by( $filter, $value, $time_field );
 			default:
 				// code...
 				break;
@@ -403,7 +405,8 @@ class LP_Order_DB extends LP_Database {
 		$filter                   = new LP_Order_Filter();
 		$filter->collection       = $this->tb_posts;
 		$filter->collection_alias = 'p';
-		$filter                   = $this->filter_time( $filter, $type, $value );
+		$time_field               = 'p.post_date';
+		$filter                   = $this->filter_time( $filter, $type, $time_field, $value );
 		$filter                   = $this->filter_order_count_statics( $filter );
 		$filter->limit            = -1;
 		$result                   = $this->execute( $filter );
