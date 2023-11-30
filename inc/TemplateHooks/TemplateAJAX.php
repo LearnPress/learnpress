308550f688d769f80c05c4a47e3f7ac343c591b3
Tungnx <tungnxt89@gmail.com>
2023-11-30 10:24:04 +0700

    = 4.2.5.7 = ~ Tweak TemplateAJAX class: comment info, validate args.

--- inc/TemplateHooks/TemplateAJAX.php
+++ inc/TemplateHooks/TemplateAJAX.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Template load via AJAX.
+ * Use for load any content via AJAX.
  *
  * @since 4.2.5.7
  * @version 1.0.0
@@ -8,29 +9,46 @@
 
 namespace LearnPress\TemplateHooks;
 
+use Exception;
 use LearnPress\Helpers\Template;
 use Throwable;
 
 class TemplateAJAX {
 	/**
-	 * @param string $content
-	 * @param array $args
-	 * @param array $callback [ 'class', 'method' ]
+	 * @param string $html_el_target html content element want to attach innerHTML.
+	 * EX: '<div id="el-want-attach-content-ajax"></div>'
+	 *
+	 * @param array $args [ 'el_target' => 'id/class' ],
+	 * id/class is target for js detect to attach content to $html_el_target, id has prefix '#', class has prefix '.'
+	 *
+	 * @param array $callback [ 'class', 'method' ] method use to render content html
 	 *
 	 * @return string
 	 * @since 4.2.5.7
 	 * @version 1.0.0
 	 */
-	public static function load_content_via_ajax( string $content = '', array $args = [], array $callback = [] ): string {
+	public static function load_content_via_ajax( string $html_el_target = '', array $args = [], array $callback = [] ): string {
 		$html_wrapper = '';
 
 		try {
+			if ( empty( $html_el_target ) ) {
+				throw new Exception( 'Missing html element target' );
+			}
+
+			if ( ! isset( $args['el_target'] ) ) {
+				throw new Exception( 'Missing args el_target' );
+			}
+
+			if ( ! isset( $callback['class'] ) || ! isset( $callback['method'] ) ) {
+				throw new Exception( 'Missing args callback class || method' );
+			}
+
 			$data         = [
 				'args'     => $args,
 				'callback' => $callback,
 			];
 			$data_send    = esc_attr( htmlentities2( json_encode( $data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE ) ) );
-			$el_has_data = sprintf( '<div class="lp-load-ajax-element" data-send="%s">', $data_send );
+			$el_has_data  = sprintf( '<div class="lp-load-ajax-element" data-send="%s">', $data_send );
 			$html_wrapper = [
 				$el_has_data => '</div>',
 			];
@@ -38,6 +56,6 @@ class TemplateAJAX {
 			error_log( __METHOD__ . ' ' . $e->getMessage() );
 		}
 
-		return Template::instance()->nest_elements( $html_wrapper, $content );
+		return Template::instance()->nest_elements( $html_wrapper, $html_el_target );
 	}
 }
