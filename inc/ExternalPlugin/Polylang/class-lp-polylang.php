e302683dca269879c28907d25388e1bb27f6b2af
Khanh Bui <khanhbd@physcode.com>
2024-01-30 14:26:24 +0700

    =4.2.6.0= ~ fix course query filter with polylang

--- inc/ExternalPlugin/Polylang/class-lp-polylang.php
+++ inc/ExternalPlugin/Polylang/class-lp-polylang.php
@@ -96,11 +96,15 @@ class LP_Polylang {
 			return $filter;
 		}
 
+		if ( $pll_current_lang == pll_default_language() ) {
+			return $filter;
+		}
+
 		$lp_db          = LP_Database::getInstance();
-		$filter->join[] = "INNER JOIN $lp_db->tb_term_relationships AS r_term ON p.ID = r_term.object_id";
-		$filter->join[] = "INNER JOIN $lp_db->tb_terms AS term ON r_term.term_taxonomy_id = term.term_id";
+		$filter->join[] = "INNER JOIN $lp_db->tb_term_relationships AS r_term_pll ON p.ID = r_term_pll.object_id";
+		$filter->join[] = "INNER JOIN $lp_db->tb_terms AS pll_terms ON r_term_pll.term_taxonomy_id = pll_terms.term_id";
 		if ( empty( LP_Request::get_param( 'term_id' ) ) ) {
-			$filter->where[] = $lp_db->wpdb->prepare( 'AND term.slug = %s', $pll_current_lang );
+			$filter->where[] = $lp_db->wpdb->prepare( 'AND pll_terms.slug = %s', $pll_current_lang );
 		}
 
 		return $filter;
