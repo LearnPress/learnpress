cede4f5730505ba84b28b262a894e128100e54d4
Tungnx <tungnxt89@gmail.com>
2024-02-15 15:05:20 +0700

    = 4.2.6.1 = ~ Tweak: wp-remote file.

--- inc/admin/views/admin-notices/wp-remote.php
+++ inc/admin/views/admin-notices/wp-remote.php
@@ -18,12 +18,20 @@ if ( ! is_wp_error( $data['check'] ) ) {
 /**
  * @var WP_Error $wp_error
  */
-$wp_error = $data['check'];
+$wp_error      = $data['check'];
 $message_error = $wp_error->get_error_message();
 ?>
 
 <div class="lp-admin-notice notice notice-error">
 	<p>
-		<?php echo sprintf( '%s %s', '<strong>wp_remote_get</strong>: ', $message_error ); ?>
+		<?php
+		echo sprintf(
+			'%s %s. <a href="%s">%s</a>',
+			'<strong>wp_remote_get</strong>: ',
+			$message_error,
+			admin_url( 'site-health.php' ),
+			__( 'Check Site Health', 'learnpress' )
+		);
+		?>
 	</p>
 </div>
