b761f2ee3ff12e01b51c3906c1f7e2f52ed97f1b
Tungnx <tungnxt89@gmail.com>
2024-03-01 09:16:09 +0700

    = 4.2.6.3 = ~ Security: remove capability unfiltered_html of Instructor.

--- inc/user/lp-user-functions.php
+++ inc/user/lp-user-functions.php
@@ -155,7 +155,7 @@ function learn_press_add_user_roles() {
 		$teacher->add_cap( 'edit_published_' . $course_cap );
 		$teacher->add_cap( 'edit_' . $course_cap );
 		$teacher->add_cap( 'delete_' . $course_cap );
-		$teacher->add_cap( 'unfiltered_html' );
+		//$teacher->add_cap( 'unfiltered_html' );
 
 		$settings->get( 'required_review' );
 
