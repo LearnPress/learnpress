ab10824693721354de0571c3610492c401e0a0b9
Tungnx <tungnxt89@gmail.com>
2024-03-05 15:02:59 +0700

    = 4.2.6.4 = ~ Tweak: get_file_size_from_url method.

--- inc/class-lp-file-system.php
+++ inc/class-lp-file-system.php
@@ -329,18 +329,31 @@ if ( ! class_exists( 'LP_WP_Filesystem' ) ) {
 
 			return $output;
 		}
+
 		public function download_url( $url, $timeout = 300, $signature_verification = false ) {
 			return download_url( $url, $timeout, $signature_verification );
 		}
+
 		public function lp_handle_upload( &$file, $overrides = false, $time = null ) {
 			return wp_handle_upload( $file, $overrides, $time );
 		}
+
+		/**
+		 * Get size of file from url
+		 *
+		 * @param $url
+		 *
+		 * @return string
+		 * @since 4.2.3
+		 * @version 1.0.1
+		 */
 		public function get_file_size_from_url( $url ) {
 			$tmp_file = $this->download_url( $url );
-			$size     = '0KB';
-			if ( $tmp_file ) {
+			$size     = '';
+			if ( $tmp_file && ! is_wp_error( $tmp_file ) ) {
 				$size = ( filesize( $tmp_file ) / 1024 < 1024 ) ? round( filesize( $tmp_file ) / 1024, 2 ) . 'KB' : round( filesize( $tmp_file ) / 1024 / 1024, 2 ) . 'MB';
 			}
+
 			return $size;
 		}
 	}
