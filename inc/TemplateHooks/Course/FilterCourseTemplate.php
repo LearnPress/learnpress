bf2fbcfeb1d80401ac43491d7a6b09b6cb116b87
Khanh Bui <khanhbd@physcode.com>
2024-04-08 12:02:19 +0700

    =4.2.6.5= ~ edit filter course sections field

--- inc/TemplateHooks/Course/FilterCourseTemplate.php
+++ inc/TemplateHooks/Course/FilterCourseTemplate.php
@@ -75,7 +75,7 @@ class FilterCourseTemplate {
 				if ( is_callable( [ $this, 'html_' . $field ] ) ) {
 					$sections[ $field ] = [ 'text_html' => $this->{'html_' . $field}( $data ) ];
 				} else { // For custom field.
-					apply_filters( 'learn-press/filter-courses/sections/field', $sections, $field, $data );
+					$sections[ $field ] = apply_filters( 'learn-press/filter-courses/sections/field/html', NULL, $field, $data );
 				}
 			}
 
