02f80cc43bad72557d522d5e12f4d1546e981643
Tungnx <tungnxt89@gmail.com>
2024-07-09 13:56:01 +0700

    = 4.2.6.9 = ~ Format code search_user.js on Order

--- assets/src/js/admin/order/search_user.js
+++ assets/src/js/admin/order/search_user.js
@@ -1,10 +1,10 @@
-import TomSelect from "tom-select";
-import { lpFetchAPI } from "../../utils.js";
-import Api from "../../api.js";
+import TomSelect from 'tom-select';
+import { lpFetchAPI } from '../../utils.js';
+import Api from '../../api.js';
 
 export default function search_user() {
-	const buildTomSelect = (elTomSelect, options, fetchAPI) => {
-		if (!elTomSelect) {
+	const buildTomSelect = ( elTomSelect, options, fetchAPI ) => {
+		if ( ! elTomSelect ) {
 			return;
 		}
 
@@ -12,74 +12,68 @@ export default function search_user() {
 			options: [],
 			plugins: {
 				remove_button: {
-					title: "Remove this item",
+					title: 'Remove this item',
 				},
 			},
-			load(keySearch, callback) {
-				fetchAPI(keySearch, callback);
+			load( keySearch, callback ) {
+				fetchAPI( keySearch, callback );
 			},
 		};
 
 		options = { ...optionDefault, ...options };
 
-		return new TomSelect(elTomSelect, options);
+		return new TomSelect( elTomSelect, options );
 	};
 
-	const fetchOrderUsers = (keySearch = "", callback, elTomSelect) => {
+	const fetchOrderUsers = ( keySearch = '', callback, elTomSelect ) => {
 		const url = Api.admin.apiSearchUsers;
 		let dataUser;
 
-		let tomOptions = {
+		const tomOptions = {
 			maxItems: null,
 			maxOptions: 10,
 			render: {
-				item: function (data, escape) {
+				item( data, escape ) {
 					return (
-						'<li data-id="' +
-						data.value +
-						'">' +
-						'<div class="item" data-ts-item="">' +
-						data.text +
-						"</div>" +
-						'<input type="hidden" name="order-customer[]" value="' +
-						data.value +
-						'">' +
-						"</li>"
+						`<li data-id="${ data.value }">` +
+						`<div class="item" data-ts-item="">${ data.text }</div>` +
+						`<input type="hidden" name="order-customer[]" value="${ data.value }">` +
+						'</li>'
 					);
 				},
 			},
 		};
 
-		const listUserEl = document.querySelector("#list-users");
-		if (listUserEl && listUserEl.dataset.userId) {
-			dataUser = JSON.parse(listUserEl.dataset.userId);
-			tomOptions["items"] = dataUser;
+		const listUserEl = document.querySelector( '#list-users' );
+		if ( listUserEl && listUserEl.dataset.userId ) {
+			dataUser = JSON.parse( listUserEl.dataset.userId );
+			tomOptions.items = dataUser;
 		}
 
 		const params = {
 			headers: {
-				"Content-Type": "application/json",
-				"X-WP-Nonce": lpDataAdmin.nonce,
+				'Content-Type': 'application/json',
+				'X-WP-Nonce': lpDataAdmin.nonce,
 			},
-			method: "POST",
-			body: JSON.stringify({ search: keySearch }),
+			method: 'POST',
+			body: JSON.stringify( { search: keySearch } ),
 		};
 
-		lpFetchAPI(url, params, {
-			success: (response) => {
-				const options = response.data.map((item) => {
+		lpFetchAPI( url, params, {
+			success: ( response ) => {
+				const options = response.data.map( ( item ) => {
 					return {
 						value: item.ID,
-						text: `${item.display_name} (#${item.ID}) - ${item.user_email}`,
+						text: `${ item.display_name } (#${ item.ID }) - ${ item.user_email }`,
 					};
-				});
+				} );
 
-				tomOptions["options"] = options;
+				tomOptions.options = options;
 
 				// Set data users default first to Tom Select.
-				if (keySearch === "") {
-					if (listUserEl) {
-						let elTomSelectUser = buildTomSelect(
+				if ( keySearch === '' ) {
+					if ( listUserEl ) {
+						const elTomSelectUser = buildTomSelect(
 							listUserEl,
 							tomOptions,
 							fetchOrderUsers,
@@ -87,22 +81,22 @@ export default function search_user() {
 					}
 				}
 
-				if ("function" === typeof callback) {
-					if (callback.name === "setupOptions") {
-						elTomSelect.setupOptions(options);
+				if ( 'function' === typeof callback ) {
+					if ( callback.name === 'setupOptions' ) {
+						elTomSelect.setupOptions( options );
 					} else {
-						callback(options);
+						callback( options );
 					}
 				}
 			},
-		});
+		} );
 	};
 
-	document.addEventListener("DOMContentLoaded", () => {
-		const listUserEl = document.querySelector("#list-users");
+	document.addEventListener( 'DOMContentLoaded', () => {
+		const listUserEl = document.querySelector( '#list-users' );
 
-		if (listUserEl) {
+		if ( listUserEl ) {
 			fetchOrderUsers();
 		}
-	});
+	} );
 }
