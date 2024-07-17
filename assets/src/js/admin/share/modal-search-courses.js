a43f0977eedce3628cd06f82638ab8d13151eeb2
DoNgocPhuc <phucngocdo@gmail.com>
2024-07-17 17:41:00 +0700

    update js

--- assets/src/js/admin/share/modal-search-courses.js
+++ assets/src/js/admin/share/modal-search-courses.js
@@ -1,74 +1,108 @@
-( function( $ ) {
+( function() {
 	'use strict';
 
-	jQuery( document ).ready( function( $ ) {
+	document.addEventListener( 'DOMContentLoaded', ( event ) => {
 		let data = {},
 			paged = 1,
 			term = '',
 			hasItems = false,
 			selectedItems = [];
 
-		const $listItems = $( '#learn-press-order .list-order-items' ).find( 'tbody' );
+		const listItems = document.querySelector( '#learn-press-order .list-order-items tbody' );
 
 		const getAddedItems = () => {
-			return $( '#learn-press-order .list-order-items tbody' ).children( '.order-item-row' ).map( function() {
-				return $( this ).data( 'id' );
-			} ).get();
+			const orderItems = document.querySelectorAll( '#learn-press-order .list-order-items tbody .order-item-row' );
+			return [ ...orderItems ].map( ( orderItem ) => {
+				return parseInt( orderItem.getAttribute( 'data-id' ) );
+			} );
 		};
 
 		const focusSearch = _.debounce( function() {
-			$( '#modal-search-items input[name="search"]' ).focus();
+			document.querySelector( '#modal-search-items input[name="search"]' ).focus();
 		}, 200 );
 
 		const mountSearchModal = () => {
-			const $modalSearchItems = $( '#learn-press-modal-search-items' );
-			const $modalContainer = $( '#container-modal-search-items' );
-			$modalContainer.html( $modalSearchItems.html() );
+			const modalSearchItems = document.querySelector( '#learn-press-modal-search-items' );
+			const modalContainer = document.querySelector( '#container-modal-search-items' );
+
+			modalContainer.innerHTML = modalSearchItems.innerHTML;
 		};
 
+		// const addQueryArgs = ( baseUrl, queryArgs ) => {
+		// 	const url = new URL( baseUrl );
+		//
+		// 	Object.keys( queryArgs ).forEach( ( key ) => {
+		// 		url.searchParams.append( key, queryArgs[ key ] );
+		// 	} );
+		//
+		// 	return url.toString();
+		// };
+
 		const search = _.debounce( function() {
-			$( '#modal-search-items' ).addClass( 'loading' );
+			document.querySelector( '#modal-search-items' ).classList.add( 'loading' );
+			const restUrl = lpGlobalSettings.rest || '';
 
-			$.post(
-				window.location.href, {
-					type: data.postType,
-					context: data.context,
-					context_id: data.contextId,
-					term,
-					paged,
-					exclude: data.exclude,
-					'lp-ajax': 'modal_search_items',
-					dataType: 'text',
-					nonce: lpGlobalSettings.nonce,
-				}, ( response ) => {
-					const jsonString = response.replace( /<-- LP_AJAX_START -->|<-- LP_AJAX_END -->/g, '' ).trim();
-					try {
-						const result = LP.parseJSON( jsonString );
-						hasItems = !! _.size( result.items );
+			const query = {
+				type: data.postType,
+				context: data.context,
+				context_id: data.contextId,
+				term,
+				paged,
+				exclude: data.exclude,
+				// nonce: lpGlobalSettings.nonce,
+			};
 
-						const $modal_search_items = $( '#modal-search-items' );
-						if ( hasItems ) {
-							$modal_search_items.find( '.search-nav' ).css( 'display', 'block' );
-						}
-
-						$modal_search_items.removeClass( 'loading' );
-
-						$modal_search_items.find( '.search-results' ).html( result.html ).find( 'input[type="checkbox"]' ).each( function() {
-							const id = parseInt( $( this ).val() );
-							if ( _.indexOf( selectedItems, id ) >= 0 ) {
-								this.checked = true;
-							}
-						} );
-
-						_.debounce( function() {
-							$modal_search_items.find( '.search-nav' ).html( result.nav ).find( 'a, span' ).addClass( 'button' ).filter( 'span' ).addClass( 'disabled' );
-						}, 10 )();
-					} catch ( e ) {
-						console.error( 'Error parsing JSON:', e );
-					}
-				}
-			).fail( function( jqXHR, textStatus, errorThrown ) {
-				console.error( 'Request failed: ' + textStatus + ', ' + errorThrown );
+			wp.apiFetch( {
+				path: wp.url.addQueryArgs( 'lp/v1/admin/tools/search-course', query ),
+				method: 'GET',
+			} ).then( ( response ) => {
+				// .then( ( response ) => {
+				// 	const jsonString = response.replace( /<-- LP_AJAX_START -->|<-- LP_AJAX_END -->/g, '' ).trim();
+				// 	try {
+				// 		const result = LP.parseJSON( jsonString );
+				// 		hasItems = !! _.size( result.items );
+				//
+				// 		const modal_search_items = document.querySelector( '#modal-search-items' );
+				// 		if ( hasItems ) {
+				// 			modal_search_items.querySelector( '.search-nav' ).style.display = 'block';
+				// 		}
+				//
+				// 		modal_search_items.classList.remove( 'loading' );
+				//
+				// 		modal_search_items.querySelector( '.search-results' ).innerHTML = result.html;
+				// 		const checkBoxNodes = modal_search_items.querySelectorAll( '.search-results input[type="checkbox"]' );
+				//
+				// 		[ ...checkBoxNodes ].map( ( checkBoxNode ) => {
+				// 			const id = parseInt( checkBoxNode.value );
+				//
+				// 			if ( _.indexOf( selectedItems, id ) >= 0 ) {
+				// 				checkBoxNode.checked = true;
+				// 			}
+				// 		} );
+				//
+				// 		_.debounce( function() {
+				// 			const searchNav = modal_search_items.querySelector( '.search-nav' );
+				// 			searchNav.innerHTML = result.nav;
+				//
+				// 			const buttons = searchNav.querySelectorAll( 'a,span' );
+				// 			[ ...buttons ].map( ( button ) => {
+				// 				button.classList.add( 'button' );
+				//
+				// 				if ( button.tagName === 'SPAN' ) {
+				// 					button.classList.add( 'disabled' );
+				// 				}
+				// 			} );
+				// 		}, 10 )();
+				// 	} catch ( e ) {
+				// 		console.error( 'Error parsing JSON:', e );
+				// 	}
+				// } )
+				// .catch( ( error ) => {
+				// 	console.error( 'Error:', error );
+				// } );
+			} ).catch( ( err ) => {
+				console.log( err );
+			} ).finally( () => {
 			} );
 		}, 500 );
 
@@ -183,11 +217,15 @@
 						try {
 							const result = LP.parseJSON( jsonString );
 
-							const $noItem = $listItems.find( '.no-order-items' ).hide();
+							const noItem = listItems.querySelector( '.no-order-items' );
+							noItem.style.display = 'none';
+							const itemHtml = result.item_html;
+							noItem.insertAdjacentHTML( 'beforebegin', itemHtml );
 
-							$( result.item_html ).insertBefore( $noItem );
-							$( '.order-subtotal' ).html( result.order_data.subtotal_html );
-							$( '.order-total' ).html( result.order_data.total_html );
+							const lpOrder = document.querySelector( '#learn-press-order' );
+							lpOrder.querySelector( '.order-subtotal' ).innerHTML = result.order_data.subtotal_html;
+							lpOrder.querySelector( '.order-total' ).innerHTML = result.order_data.total_html;
+
 							removeModal();
 						} catch ( e ) {
 							console.error( 'Error parsing JSON:', e );
@@ -223,22 +261,30 @@
 			}
 		};
 
-		$( '#learn-press-add-order-item' ).on( 'click', function() {
-			data = {
-				postType: 'lp_course',
-				context: 'order-items',
-				contextId: $( '#post_ID' ).val(),
-				exclude: getAddedItems(),
-				show: true,
-			};
+		const addOrderItemBtn = document.querySelector( '#learn-press-add-order-item' );
+		if ( addOrderItemBtn ) {
+			addOrderItemBtn.addEventListener( 'click', function( event ) {
+				data = {
+					postType: 'lp_course',
+					context: 'order-items',
+					exclude: getAddedItems(),
+					show: true,
+				};
 
-			term = '';
-			paged = 1;
+				const postIdNode = document.querySelector( '#post_ID' );
 
-			mountSearchModal();
-			focusSearch();
-			search();
-		} );
+				if ( postIdNode ) {
+					data.contextId = postIdNode.value;
+				}
+
+				term = '';
+				paged = 1;
+
+				mountSearchModal();
+				focusSearch();
+				search();
+			} );
+		}
 
 		doSearch();
 		loadPage();
@@ -246,5 +292,5 @@
 		addItems();
 		closeModal();
 	} );
-}( jQuery ) );
+}() );
 
