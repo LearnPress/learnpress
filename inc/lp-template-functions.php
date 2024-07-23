7d1cdbf648939aa5f821a5cea39de4f87ff8c98c
Tungnx <tungnxt89@gmail.com>
2024-07-23 16:00:14 +0700

    = 4.2.6.9 =

--- inc/lp-template-functions.php
+++ inc/lp-template-functions.php
@@ -88,7 +88,7 @@ if ( ! function_exists( 'learn_press_get_course_tabs' ) ) {
 		}
 
 		$can_show_tab_material = false;
-		if ( empty( $course->is_no_required_enroll() )
+		if ( $course->is_no_required_enroll()
 			|| $user->has_enrolled_or_finished( $course->get_id() )
 			|| $user->is_instructor() || $user->is_admin() ) {
 			$can_show_tab_material = true;
