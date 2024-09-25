1e11d9e644b9ebaab89fe50668a023c63d57d788
Khanh Bui <khanhbd@physcode.com>
2024-09-25 14:17:43 +0700

    =4.7.0= ~ display cover image in profile page and check filetype

--- templates/profile/sidebar/header.php
+++ templates/profile/sidebar/header.php
@@ -1,4 +1,13 @@
-<div class="wrapper-profile-header wrap-fullwidth">
+<?php
+$profile   = LP_Profile::instance();
+$image_url = $profile->get_cover_image_src();
+$style     = '';
+if ( ! empty( $image_url ) ) {
+	$style = "background-image: url($image_url); background-repeat: no-repeat; background-position: center; background-size: cover;";
+	$style = htmlspecialchars( $style, ENT_QUOTES, 'UTF-8' );
+}
+?>
+<div class="wrapper-profile-header wrap-fullwidth" style="<?php esc_attr_e( $style ); ?>" >
 	<div class="lp-content-area lp-profile-content-area">
 		<?php do_action( 'learn-press/user-profile-account' ); ?>
 	</div>
--- templates/profile/tabs/settings/cover-image.php
+++ templates/profile/tabs/settings/cover-image.php
@@ -10,24 +10,16 @@
  */
 
 defined( 'ABSPATH' ) || exit();
+$profile   = LP_Profile::instance();
+$image_url = $profile->get_cover_image_src();
 ?>
-<!-- <link href="https://unpkg.com/cropperjs/dist/cropper.min.css" rel="stylesheet"> -->
-<style type="text/css">
-	.lp-upload-button-wrapper {
-		display:flex;
-		gap: 1em;
-	}
-	#lp-cover-image-upload .lp-upload-button-wrapper button {
-		margin:1em 0;
-	}
-</style>
-<div id="lp-cover-image-upload">
+<div id="lp-cover-image-upload" class="learnpress-page">
 	<div class="lp-cover-image-wrapper">
-		<img src="" id="lp-cover-image" />
+		<img src="<?php esc_attr_e( $image_url ); ?>" id="lp-cover-image" />
 	</div>
 	<div class="lp-upload-button-wrapper">
-		<input id="lp-cover-image-file" type="file" name="lp-cover-image" hidden />
+		<input id="lp-cover-image-file" type="file" name="lp-cover-image" accept="image/png, image/jpeg, image/webp" hidden />
 		<button id="lp-upload-cover-image" class="lp-button"><?php esc_html_e( 'Upload', 'learnpress' ); ?></button>
-		<button id="lp-save-cover-image" class="lp-button"><?php esc_html_e( 'Save', 'learnpress' ) ?></button>
+		<button id="lp-save-cover-image" class="lp-button"><?php esc_html_e( 'Save', 'learnpress' ); ?></button>
 	</div>
 </div>
