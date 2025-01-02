7fe5ec080274a78bf02a79549e4622378cee3364
VuxMinhThanh <vuxminhthanh@gmail.com>
2025-01-02 17:14:32 +0700

    Update template archive course

--- assets/src/apps/js/blocks/widget-archive-course/lp-archive-course/edit.js
+++ assets/src/apps/js/blocks/widget-archive-course/lp-archive-course/edit.js
@@ -5,69 +5,121 @@ export const edit = ( props ) => {
 	const TEMPLATE = [
 		[
 			'core/group',
-			{
-				className: 'lp-archive-courses',
-				layout: {
-					type: 'default',
-				},
-			},
+			{ className: 'lp-archive-courses', layout: { type: 'default' } },
 			[
-				[ 'learnpress/breadcrumb' ],
-
+				[ 'learnpress/breadcrumb', {} ],
 				[
 					'core/group',
 					{
-						className: 'lp-content-area',
+						className: 'lp-content-area has-sidebar',
 						layout: {
-							type: 'default',
+							type: 'flex',
+							orientation: 'horizontal',
+							flexWrap: 'nowrap',
+							verticalAlignment: 'top',
+							justifyContent: 'left',
 						},
 					},
 					[
 						[
 							'core/group',
 							{
-								tagName: 'header',
-								className: 'learn-press-courses-header',
-								layout: {
-									type: 'default',
-								},
-							},
-							[
-								[
-									'core/heading',
-									{
-										level: 1,
-										content: 'All Courses',
-										className: 'wp-block-heading',
-									},
-								],
-							],
-						],
-
-						[
-							'learnpress/list-course-archive-course',
-							{
-								custom: true,
+								className: 'lp-content-area',
+								layout: { type: 'default' },
 							},
 							[
 								[
 									'core/group',
 									{
-										className: 'lp-courses-bar',
-										layout: {
-											type: 'flex',
-											flexWrap: 'nowrap',
-										},
+										tagName: 'header',
+										className: 'learn-press-courses-header',
+										layout: { type: 'default' },
 									},
 									[
-										[ 'learnpress/search-archive-course' ],
-										[ 'learnpress/order-by-archive-course' ],
-										[ 'learnpress/switch-layout-archive-course' ],
+										[
+											'core/heading',
+											{
+												level: 1,
+												className: 'wp-block-heading',
+												content: 'All Courses',
+											},
+										],
 									],
 								],
-								[ 'learnpress/template-course-archive-course' ],
+								[
+									'learnpress/list-course-archive-course',
+									{},
+									[
+										[
+											'core/group',
+											{
+												className: 'lp-courses-bar',
+												layout: {
+													type: 'flex',
+													flexWrap: 'nowrap',
+												},
+											},
+											[
+												[
+													'learnpress/search-archive-course',
+													{},
+												],
+												[
+													'learnpress/order-by-archive-course',
+													{},
+												],
+												[
+													'learnpress/switch-layout-archive-course',
+													{},
+												],
+											],
+										],
+										[
+											'learnpress/template-course-archive-course',
+											{},
+											[
+												[
+													'learnpress/media-course-archive-course',
+													{
+														content:
+															'{{media-course}}',
+													},
+												],
+												[
+													'learnpress/title-course-archive-course',
+													{
+														content:
+															'{{title-course}}',
+													},
+												],
+												[
+													'learnpress/instructor-category-course-archive-course',
+													{
+														content:
+															'{{instructor-category-course}}',
+													},
+												],
+												[
+													'learnpress/meta-course-archive-course',
+													{
+														content:
+															'{{meta-course}}',
+													},
+												],
+												[
+													'learnpress/info-course-archive-course',
+													{
+														content:
+															'{{info-course}}',
+													},
+												],
+											],
+										],
+									],
+								],
 							],
 						],
+						[ 'learnpress/sidebar-archive-course', {} ],
 					],
 				],
 			],
