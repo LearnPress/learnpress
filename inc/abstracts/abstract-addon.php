9b2f18dd661725c6049c86debee28ec294a3f119
Tungnx <tungnxt89@gmail.com>
2025-01-23 08:58:05 +0700

    = 4.2.7.6 =

--- inc/abstracts/abstract-addon.php
+++ inc/abstracts/abstract-addon.php
@@ -156,7 +156,7 @@ class LP_Addon {
 	/**
 	 * Init add-on hooks.
 	 *
-	 * @depecated 4.2.7.6
+	 * @deprecated 4.2.7.6
 	 * Need remove this function, don't use on the addons news to controls hook.
 	 */
 	protected function _init_hooks() {
