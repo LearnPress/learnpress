f0506bda3ad920bce9073996691e866201b0079d
VuxMinhThanh <vuxminhthanh@gmail.com>
2025-02-27 15:22:39 +0700

    Update setup template lp-single-course block

--- assets/src/apps/js/blocks/widget-single-course/lp-single-course/edit.js
+++ assets/src/apps/js/blocks/widget-single-course/lp-single-course/edit.js
@@ -5,262 +5,154 @@ export const edit = ( props ) => {
 	const TEMPLATE = [
 		[
 			'core/group',
-			{ layout: { type: 'constrained' } },
-			[
-				[ 'learnpress/breadcrumb', {} ],
-				[ 'learnpress/item-curriculum-course', {} ],
-			],
-		],
-		[
-			'core/group',
 			{
-				metadata: { name: 'Course Summary' },
-				className: 'course-summary',
-				layout: { type: 'constrained' },
+				metadata: { name: 'Header' },
+				className: 'lp-single-course__header',
+				layout: { type: 'default' },
 			},
 			[
 				[
 					'core/group',
 					{
-						metadata: { name: 'Content' },
-						className: 'course-content course-summary-content',
-						layout: { type: 'constrained' },
+						metadata: { name: 'Inner' },
+						className: 'lp-single-course__header__inner',
+						layout: { type: 'default' },
+					},
+					[
+						[ 'learnpress/breadcrumb', {} ],
+						[ 'learnpress/title-single-course', { tag: 'h1' } ],
+						[
+							'core/group',
+							{
+								metadata: { name: 'Meta Left' },
+								className: 'course-instructor-category',
+								layout: { type: 'flex', flexWrap: 'nowrap' },
+							},
+							[
+								[ 'learnpress/instructor-single-course', {} ],
+								[ 'learnpress/categories-single-course', {} ],
+							],
+						],
+						[ 'learnpress/info-one-course', {} ],
+					],
+				],
+			],
+		],
+		[
+			'core/group',
+			{
+				metadata: { name: 'Area' },
+				className: 'lp-content-area',
+				layout: { type: 'default' },
+			},
+			[
+				[
+					'core/group',
+					{
+						metadata: { name: 'Main' },
+						className: 'lp-single-course-main',
+						layout: { type: 'default' },
 					},
 					[
 						[
 							'core/group',
 							{
-								metadata: { name: 'Course Detail Info' },
-								className: 'course-detail-info',
-								layout: { type: 'constrained' },
+								metadata: { name: 'Left' },
+								className: 'lp-single-course-main__left',
+								layout: { type: 'default' },
 							},
 							[
+								[ 'learnpress/description-single-course', {} ],
+								[ 'learnpress/features-single-course', {} ],
 								[
-									'core/group',
-									{
-										className: 'lp-content-area',
-										layout: { type: 'constrained' },
-									},
-									[
-										[
-											'core/group',
-											{ layout: { type: 'constrained' } },
-											[
-												[
-													'core/group',
-													{
-														className:
-															'course-info-left',
-														layout: {
-															type: 'constrained',
-														},
-													},
-													[
-														[
-															'core/group',
-															{
-																metadata: {
-																	name: 'Meta Primary',
-																},
-																className:
-																	'course-meta course-meta-primary',
-																layout: {
-																	type: 'constrained',
-																},
-															},
-															[
-																[
-																	'core/group',
-																	{
-																		metadata:
-																			{
-																				name: 'Meta Left',
-																			},
-																		className:
-																			'course-meta__pull-left',
-																		layout: {
-																			type: 'constrained',
-																		},
-																	},
-																	[
-																		[
-																			'learnpress/instructor-single-course',
-																			{},
-																		],
-																		[
-																			'learnpress/categories-single-course',
-																			{},
-																		],
-																	],
-																],
-															],
-														],
-														[
-															'learnpress/title-single-course',
-															{ tag: 'h1' },
-														],
-														[
-															'core/group',
-															{
-																metadata: {
-																	name: 'Course Meta Secondary',
-																},
-																className:
-																	'course-meta course-meta-secondary',
-																layout: {
-																	type: 'constrained',
-																},
-															},
-															[
-																[
-																	'core/group',
-																	{
-																		className:
-																			'course-meta__pull-left',
-																		layout: {
-																			type: 'constrained',
-																		},
-																	},
-																	[
-																		[
-																			'learnpress/duration-single-course',
-																			{},
-																		],
-																		[
-																			'learnpress/level-single-course',
-																			{},
-																		],
-																		[
-																			'learnpress/lesson-single-course',
-																			{},
-																		],
-																		[
-																			'learnpress/quiz-single-course',
-																			{},
-																		],
-																		[
-																			'learnpress/student-single-course',
-																			{},
-																		],
-																	],
-																],
-															],
-														],
-													],
-												],
-											],
-										],
-									],
+									'learnpress/target-audiences-single-course',
+									{},
 								],
+								[ 'learnpress/requirements-single-course', {} ],
+								[ 'learnpress/faqs-single-course', {} ],
+								[ 'learnpress/curriculum-single-course', {} ],
+								[
+									'learnpress/instructor-section-single-course',
+									{},
+								],
+								[ 'learnpress/comment', {} ],
 							],
 						],
 						[
 							'core/group',
 							{
-								metadata: { name: 'Content Area' },
-								className: 'lp-entry-content lp-content-area',
-								layout: { type: 'constrained' },
+								metadata: { name: 'Right' },
+								className: 'lp-single-course-main__right',
+								layout: { type: 'default' },
 							},
 							[
 								[
 									'core/group',
 									{
-										metadata: { name: 'Content Left' },
-										className: 'entry-content-left',
-										layout: { type: 'constrained' },
-									},
-									[
-										[ 'learnpress/tabs-single-course', {} ],
-										[
-											'learnpress/requirements-single-course',
-											{},
-										],
-										[
-											'learnpress/features-single-course',
-											{},
-										],
-										[
-											'learnpress/target-audiences-single-course',
-											{},
-										],
-										[ 'learnpress/comment', {} ],
-									],
-								],
-								[
-									'core/group',
-									{
-										tagName: 'aside',
-										metadata: {
-											name: 'Course Summary SideBar',
-										},
 										className:
-											'course-summary-sidebar slide-top',
-										layout: { type: 'constrained' },
+											'lp-single-course-main__right__inner',
+										layout: { type: 'default' },
 									},
 									[
+										[ 'learnpress/image-single-course', {} ],
+										[ 'learnpress/price-single-course', {} ],
+										[
+											'learnpress/progress-single-course',
+											{},
+										],
 										[
 											'core/group',
 											{
-												className:
-													'course-summary-sidebar__inner',
-												layout: { type: 'constrained' },
+												metadata: { name: 'Meta' },
+												className: 'info-metas',
+												layout: { type: 'default' },
 											},
 											[
 												[
-													'core/group',
-													{
-														className:
-															'course-sidebar-top',
-														layout: {
-															type: 'constrained',
-														},
-													},
-													[
-														[
-															'core/group',
-															{
-																className:
-																	'course-sidebar-preview',
-																layout: {
-																	type: 'constrained',
-																},
-															},
-															[
-																[
-																	'learnpress/image-single-course',
-																	{},
-																],
-																[
-																	'learnpress/price-single-course',
-																	{},
-																],
-																[
-																	'learnpress/btn-purchase-single-course',
-																	{},
-																],
-																[
-																	'learnpress/time-single-course',
-																	{},
-																],
-																[
-																	'learnpress/progress-single-course',
-																	{},
-																],
-																[
-																	'learnpress/feature-review-single-course',
-																	{},
-																],
-															],
-														],
-													],
+													'learnpress/student-single-course',
+													{},
+												],
+												[
+													'learnpress/lesson-single-course',
+													{},
+												],
+												[
+													'learnpress/duration-single-course',
+													{},
+												],
+												[
+													'learnpress/quiz-single-course',
+													{},
+												],
+												[
+													'learnpress/level-single-course',
+													{},
 												],
 											],
+										],
+										[
+											'learnpress/btn-purchase-single-course',
+											{},
+										],
+										[ 'learnpress/share-course', {} ],
+										[
+											'learnpress/feature-review-single-course',
+											{},
 										],
 									],
 								],
 							],
 						],
 					],
+				],
+				[
+					'core/group',
+					{
+						metadata: { name: 'Related' },
+						className: 'lp-list-courses-related',
+						layout: { type: 'default' },
+					},
+					[ [ 'learnpress/related-course', {} ] ],
 				],
 			],
 		],
