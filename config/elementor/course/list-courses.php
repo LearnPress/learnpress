c8b9d867a6eaff52cd139bc59ec0f129a5808ef0
Tungnx <tungnxt89@gmail.com>
2025-03-31 10:07:02 +0700

    = 4.2.8.2 =

--- config/elementor/course/list-courses.php
+++ config/elementor/course/list-courses.php
@@ -14,7 +14,7 @@ $filter              = new LP_Course_Filter();
 $filter->limit       = -1;
 $filter->only_fields = array( 'ID', 'post_title' );
 $filter->post_status = [ 'publish' ];
-$courses_obj         = Courses::get_courses( $filter );
+$courses_obj         = (array) Courses::get_courses( $filter );
 $courses             = [];
 $categories          = [];
 
