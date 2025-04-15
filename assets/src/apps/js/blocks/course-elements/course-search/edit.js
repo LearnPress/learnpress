15727dd764af285ff6cef70501d34fb67d551f3b
VuxMinhThanh <vuxminhthanh@gmail.com>
2025-04-15 11:44:05 +0700

    Update preview block

--- assets/src/apps/js/blocks/course-elements/course-search/edit.js
+++ assets/src/apps/js/blocks/course-elements/course-search/edit.js
@@ -7,7 +7,7 @@ export const edit = ( props ) => {
 			<div { ...blockProps }>
 				<form className="block-search-courses" >
 					<input type="search" placeholder="Search courses..." name="c_search" value="" />
-					<button type="submit" name="lp-btn-search-courses"><i className="lp-icon-search"></i></button>
+					<button type="submit" disabled name="lp-btn-search-courses"><i className="lp-icon-search"></i></button>
 				</form>
 			</div>
 		</>
--- inc/rest-api/v1/frontend/class-lp-rest-courses-controller.php
+++ inc/rest-api/v1/frontend/class-lp-rest-courses-controller.php
@@ -214,7 +214,7 @@ class LP_REST_Courses_Controller extends LP_Abstract_REST_Controller {
 				$courseItem->image             = $singleCourseTemplate->html_image( $courseModel );
 				$courseItem->instructor        = $singleCourseTemplate->html_instructor( $courseModel, false, [ 'is_link' => 'false' ] );
 				$courseItem->category          = $singleCourseTemplate->html_categories( $courseModel );
-				$courseItem->button            = sprintf( '<div class="course-readmore"><a href="%s">%s</a></div>', $courseModel->get_permalink(), __( 'Read more', 'learnpress' ) );
+				$courseItem->button            = sprintf( '<div class="course-readmore"><a >%s</a></div>', __( 'Read more', 'learnpress' ) );
 
 				$courses[] = apply_filters( 'lp/rest-api/frontend/course/archive_course/courses', $courseItem, $courseModel );
 			}
