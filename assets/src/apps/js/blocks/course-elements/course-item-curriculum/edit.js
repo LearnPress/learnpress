8925b86aa163a7204da527dfca2220b8bb3ce677
VuxMinhThanh <vuxminhthanh@gmail.com>
2025-04-17 16:44:40 +0700

    Update preview course item curriculum

--- assets/src/apps/js/blocks/course-elements/course-item-curriculum/edit.js
+++ assets/src/apps/js/blocks/course-elements/course-item-curriculum/edit.js
@@ -1,10 +1,22 @@
 import { __ } from '@wordpress/i18n';
+import { Placeholder } from '@wordpress/components';
 import { useBlockProps } from '@wordpress/block-editor';
 export const edit = ( props ) => {
 	const blockProps = useBlockProps();
 	return (
 		<div {...blockProps}>
-		{ __( 'Course item curriculum', 'learnpress' ) }
+			<Placeholder
+				label={ __( 'Course item curriculum', 'learnpress' ) }
+			>
+				<div>
+					{
+						__(
+							'Displays the course curriculum, including lessons, quizzes, and other learning items, organized by sections or topics!',
+							'learnpress'
+						)
+					}
+				</div>
+			</Placeholder>
 		</div>
 	);
 };
