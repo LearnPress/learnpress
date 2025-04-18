3debbd1a3a315494226bf7ee7519e0c000f84e7b
Tungnx <tungnxt89@gmail.com>
2025-04-18 10:40:39 +0700

    = gu-block = ~ Fixed: align for CourseButtonBlockType on js.

--- assets/src/apps/js/blocks/course-elements/course-button/edit.js
+++ assets/src/apps/js/blocks/course-elements/course-button/edit.js
@@ -5,12 +5,14 @@ const Edit = ( props ) => {
 	const blockProps = useBlockProps();
 	const { attributes, setAttributes, context } = props;
 	const { lpCourseData } = context;
+	// classOfDiv to fix align.
+	let classOfDiv = blockProps.className;
+	classOfDiv = classOfDiv.replaceAll( 'wp-block-learnpress-course-button', '' );
+
 	return (
-		<>
-			<div className="course-buttons">
-				<button { ...blockProps }>{ __( 'Buy Now', 'learnpress' ) }</button>
-			</div>
-		</>
+		<div className={ classOfDiv }>
+			<button { ...blockProps }>{ __( 'Buy Now', 'learnpress' ) }</button>
+		</div>
 	);
 };
 
