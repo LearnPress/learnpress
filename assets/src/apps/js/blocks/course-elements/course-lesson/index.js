9316935c7d96693d7ceb2ebfcc641b6965b48c5d
VuxMinhThanh <vuxminhthanh@gmail.com>
2025-05-06 10:36:36 +0700

    Update course lesson block

--- assets/src/apps/js/blocks/course-elements/course-lesson/index.js
+++ assets/src/apps/js/blocks/course-elements/course-lesson/index.js
@@ -1,28 +1,24 @@
 /**
  * Register block course lesson.
  */
+
 import edit from './edit';
 import { save } from './save';
 import metadata from './block.json';
 import { registerBlockType } from '@wordpress/blocks';
 import { checkTemplatesCanLoadBlock } from '../../utilBlock.js';
 
-const block_name = 'learnpress/course-lesson';
 const templatesName = [ 'learnpress/learnpress//single-lp_course' ];
 
-/**
- * Check if the block can load in the template editor: single-lp_course.
- * if it is editing on this template, set ancestor to null
- */
-checkTemplatesCanLoadBlock( templatesName, block_name, metadata, ( metadataNew ) => {
-	registerBlockType( block_name, {
+checkTemplatesCanLoadBlock( templatesName, metadata, ( metadataNew ) => {
+	registerBlockType( metadataNew.name, {
 		...metadataNew,
 		edit,
 		save,
 	} );
 } );
 
-registerBlockType( block_name, {
+registerBlockType( metadata.name, {
 	...metadata,
 	edit,
 	save,
