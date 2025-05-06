725ed8981253aa5fb287329467f90c863b34879b
VuxMinhThanh <vuxminhthanh@gmail.com>
2025-05-06 11:52:32 +0700

    Update single course

--- assets/src/apps/js/blocks/single-course-legacy/index.js
+++ assets/src/apps/js/blocks/single-course-legacy/index.js
@@ -6,7 +6,7 @@ import { save } from './save';
 import metadata from './block.json';
 import { registerBlockType } from '@wordpress/blocks';
 
-registerBlockType( 'learnpress/single-course-legacy', {
+registerBlockType( metadata.name, {
 	...metadata,
 	edit,
 	save,
