989ceb06f60d00d710af7ea66fa5387700535e43
VuxMinhThanh <vuxminhthanh@gmail.com>
2025-05-07 17:47:29 +0700

    Update preview block course item in admin

--- assets/src/apps/js/blocks/single-course-item/item-comment/edit.js
+++ assets/src/apps/js/blocks/single-course-item/item-comment/edit.js
@@ -7,10 +7,25 @@ const Edit = ( props ) => {
 
 	return (
 		<div { ...blockProps }>
-			<Placeholder
-				label={ __( 'Item Comment', 'learnpress' ) }
-			>
-			</Placeholder>
+			<div className="learn-press-comments">
+				<div className="comment-respond">
+					<h3 className="comment-reply-title">Leave a Reply</h3>
+					<p class="comment-form-comment">
+						<label for="comment">
+							Comment <span class="required">*</span>
+						</label>{ ' ' }
+						<textarea name="comment" cols="45" rows="8" maxlength="65525"></textarea>
+					</p>
+					<p class="form-submit wp-block-button">
+						<input
+							name="submit"
+							type="submit"
+							class="submit wp-element-button"
+							value="Post Comment"
+						/>
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
