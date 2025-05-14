2f255bc0cbd8c9e38a2ad85656cff6b9f0b5158e
VuxMinhThanh <vuxminhthanh@gmail.com>
2025-05-14 17:11:46 +0700

    Update template single course offline

--- config/gutenberg/block-templates.php
+++ config/gutenberg/block-templates.php
@@ -11,6 +11,7 @@ use LearnPress\Gutenberg\Templates\ArchiveCoursesBlockCategoryTemplate;
 use LearnPress\Gutenberg\Templates\ArchiveCoursesBlockTagTemplate;
 use LearnPress\Gutenberg\Templates\SingleCourseBlockTemplate;
 use LearnPress\Gutenberg\Templates\SingleCourseItemBlockTemplate;
+use LearnPress\Gutenberg\Templates\SingleCourseOfflineBlockTemplate;
 
 return apply_filters(
 	'learn-press/config/block-templates',
@@ -20,5 +21,6 @@ return apply_filters(
 		new ArchiveCoursesBlockCategoryTemplate(),
 		new ArchiveCoursesBlockTagTemplate(),
 		new SingleCourseItemBlockTemplate(),
+		new SingleCourseOfflineBlockTemplate(),
 	]
 );
