52bfd6cc8bd98894af99d9a8bba31c972f3a40c0
Tungnx <tungnxt89@gmail.com>
2025-06-06 17:37:57 +0700

    ~ Tweak: js sidebar, comment old code for old curriculum.

--- assets/src/apps/js/frontend/single-curriculum/components/sidebar.js
+++ assets/src/apps/js/frontend/single-curriculum/components/sidebar.js
@@ -10,19 +10,9 @@ export const Sidebar = () => {
 		$( 'body' ).removeClass( 'lp-sidebar-toggle__open' );
 		$( 'body' ).removeClass( 'lp-sidebar-toggle__close' );
 
-		const wrapper = document.querySelector( '.js-block-sidebar' );
-		const computedBasis =
-			wrapper.dataset.originalFlexBasis || window.getComputedStyle( wrapper ).flexBasis;
-
-		if ( ! wrapper.dataset.originalFlexBasis && computedBasis !== '0px' ) {
-			wrapper.dataset.originalFlexBasis = computedBasis;
-		}
-
 		if ( toggle ) {
-			wrapper.style.flexBasis = '0';
 			$( 'body' ).addClass( 'lp-sidebar-toggle__close' );
 		} else {
-			wrapper.style.flexBasis = computedBasis || 'auto';
 			$( 'body' ).addClass( 'lp-sidebar-toggle__open' );
 		}
 	};
@@ -32,40 +22,28 @@ export const Sidebar = () => {
 		if ( $( window ).innerWidth() <= 768 ) {
 			elSidebarToggle.setAttribute( 'checked', 'checked' );
 		} else if ( LP.Cookies.get( 'sidebar-toggle' ) ) {
-			const wrapper = document.querySelector( '.js-block-sidebar' );
-			const computedBasis =
-				wrapper.dataset.originalFlexBasis || window.getComputedStyle( wrapper ).flexBasis;
-
-			if ( ! wrapper.dataset.originalFlexBasis && computedBasis !== '0px' ) {
-				wrapper.dataset.originalFlexBasis = computedBasis;
-			}
-			wrapper.style.flexBasis = '0';
-
 			elSidebarToggle.setAttribute( 'checked', 'checked' );
 		} else {
 			elSidebarToggle.removeAttribute( 'checked' );
 		}
-
-		document.querySelector( '#sidebar-toggle' ).addEventListener( 'click', ( e ) => {
-			LP.Cookies.set( 'sidebar-toggle', e.target.checked ? true : false );
-			toggleSidebar( LP.Cookies.get( 'sidebar-toggle' ) );
-		} );
 	}
 	// End editor by tungnx
 
-	const $curriculum = $( '#learn-press-course-curriculum' );
+	// Code for old curriculum
+	/*const $curriculum = $( '#learn-press-course-curriculum' );
 	$curriculum.find( '.section-desc' ).each( ( i, el ) => {
 		const a = $( '<span class="show-desc"></span>' ).on( 'click', () => {
 			b.toggleClass( 'c' );
 		} );
 		const b = $( el ).siblings( '.section-title' ).append( a );
-	} );
+	} );*/
 
-	$( '.section' ).each( function () {
+	// Code for old curriculum
+	/*$( '.section' ).each( function() {
 		const $section = $( this ),
 			$toggle = $section.find( '.section-left' );
 
-		$toggle.on( 'click', function () {
+		$toggle.on( 'click', function() {
 			const isClose = $section.toggleClass( 'closed' ).hasClass( 'closed' );
 			const sections = LP.Cookies.get( 'closed-section-' + lpGlobalSettings.post_id ) || [];
 			const sectionId = parseInt( $section.data( 'section-id' ) );
@@ -82,5 +60,13 @@ export const Sidebar = () => {
 			LP.Cookies.remove( 'closed-section-(.*)' );
 			LP.Cookies.set( 'closed-section-' + lpGlobalSettings.post_id, [ ...new Set( sections ) ] );
 		} );
+	} );*/
+
+	document.addEventListener( 'click', ( e ) => {
+		if ( e.target.id === 'sidebar-toggle' ) {
+			LP.Cookies.set( 'sidebar-toggle', e.target.checked ? true : false );
+			toggleSidebar( LP.Cookies.get( 'sidebar-toggle' ) );
+		}
 	} );
 };
+
