fd3e3ae013d525c978cff087769dc67b25b4b857
Tungnx <tungnxt89@gmail.com>
2025-07-15 09:27:30 +0700

    ~ Tweak: AbstractAjax, check refer if invalid nonce

--- inc/Ajax/AbstractAjax.php
+++ inc/Ajax/AbstractAjax.php
@@ -3,7 +3,7 @@
  * class AjaxBase
  *
  * @since 4.2.7.6
- * @version 1.0.1
+ * @version 1.0.2
  */
 
 namespace LearnPress\Ajax;
@@ -29,6 +29,12 @@ abstract class AbstractAjax {
 			if ( ! wp_verify_nonce( $nonce, 'wp_rest' ) ) {
 				if ( ! in_array( get_class( $class ), $class_no_nonce ) ) {
 					wp_die( 'Invalid request!', 400 );
+				} else {
+					// Allow to handle without nonce, but must same domain.
+					$referer = wp_get_referer();
+					if ( empty( $referer ) || strpos( $referer, home_url() ) !== 0 ) {
+						wp_die( 'Invalid request!', 400 );
+					}
 				}
 			}
 
