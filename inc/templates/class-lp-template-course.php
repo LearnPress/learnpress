ea0fd0a82e4fdd3fa98909eeaded8c1b8da15041
Tungnx <tungnxt89@gmail.com>
2025-09-18 16:11:48 +0700

    = 4.2.9.3 = ~ Fixed: external link click to another tab - old code.

--- inc/templates/class-lp-template-course.php
+++ inc/templates/class-lp-template-course.php
@@ -491,23 +491,7 @@ class LP_Template_Course extends LP_Abstract_Template {
 		if ( $user && ! $user->has_enrolled_or_finished( $course->get_id() ) ) {
 			// Remove all another buttons
 			learn_press_remove_course_buttons();
-			//learn_press_get_template( 'single-course/buttons/external-link.php' );
-
-			$button_text = apply_filters( 'learn-press/external-course-button-text', $course->get_external_link_text() );
-			$nonce       = wp_create_nonce( 'external-link-' . $link );
-			$html        = sprintf(
-				'<form name="course-external-link" class="course-external-link form-button lp-form">
-					<a href="%s" target="_blank" data-id="%d" data-nonce="%s">
-						<button type="button" class="lp-button button">%s</button>
-					</a>
-				</form>',
-				esc_url( $link ),
-				esc_attr( $course->get_id() ),
-				esc_attr( $nonce ),
-				esc_html( $button_text )
-			);
-
-			echo $html;
+			learn_press_get_template( 'single-course/buttons/external-link.php' );
 
 			// Add back other buttons for other courses
 			add_action( 'learn-press/after-course-buttons', 'learn_press_add_course_buttons' );
