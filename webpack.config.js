c7ef591ef3f81a6a5d96d5b13525d0947e6cee0e
Tungnx <tungnxt89@gmail.com>
2025-11-06 17:05:17 +0700

    ~ Comment presets: [ '@babel/preset-env' ], build error.

--- webpack.config.js
+++ webpack.config.js
@@ -201,7 +201,7 @@ module.exports = {
 				use: {
 					loader: 'babel-loader',
 					options: {
-						presets: [ '@babel/preset-env' ],
+						// presets: [ '@babel/preset-env' ],
 					},
 				},
 			},
