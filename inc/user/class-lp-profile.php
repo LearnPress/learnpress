4a411e71403bcb5348c42be425ace74f6c0a3036
Tungnx <tungnxt89@gmail.com>
2026-01-19 10:49:52 +0700

    ~ LP_Profile: tweak query_courses, deprecated get_user_orders

--- inc/user/class-lp-profile.php
+++ inc/user/class-lp-profile.php
@@ -1,5 +1,7 @@
 <?php
 
+use LearnPress\Databases\UserItemsDB;
+use LearnPress\Filters\UserItemsFilter;
 use LearnPress\Helpers\Config;
 use LearnPress\Models\Courses;
 use LearnPress\Models\UserItems\UserCourseModel;
@@ -562,8 +564,12 @@ if ( ! class_exists( 'LP_Profile' ) ) {
 		 *
 		 * @return array
 		 * @since 3.0.0
+		 * @deprecated 4.2.3.x
 		 */
 		public function get_user_orders( $args = '' ) {
+			_deprecated_function( __METHOD__, '4.2.3.x' );
+			return [];
+
 			$args = wp_parse_args(
 				$args,
 				array(
@@ -623,7 +629,12 @@ if ( ! class_exists( 'LP_Profile' ) ) {
 					'post_type'      => LP_ORDER_CPT,
 					'posts_per_page' => $args['limit'],
 					'offset'         => $offset,
-					'post_status'    => [ LP_ORDER_COMPLETED_DB, LP_ORDER_PENDING_DB, LP_ORDER_PROCESSING_DB, LP_ORDER_CANCELLED_DB ],
+					'post_status'    => [
+						LP_ORDER_COMPLETED_DB,
+						LP_ORDER_PENDING_DB,
+						LP_ORDER_PROCESSING_DB,
+						LP_ORDER_CANCELLED_DB,
+					],
 					//'post__in'       => array_keys( $order_ids ),
 					//'orderby'        => 'post__in',
 					'fields'         => 'ids',
@@ -682,6 +693,7 @@ if ( ! class_exists( 'LP_Profile' ) ) {
 		 * @param array $args - Optional.
 		 *
 		 * @return LP_Query_List_Table
+		 * @throws Exception
 		 */
 		public function query_courses( string $type = 'own', array $args = array() ): LP_Query_List_Table {
 			$lp_user_items_db = LP_User_Items_DB::getInstance();
@@ -689,28 +701,37 @@ if ( ! class_exists( 'LP_Profile' ) ) {
 
 			switch ( $type ) {
 				case 'purchased':
-					// $query = $this->_curd->query_purchased_courses( $this->get_user_data( 'id' ), $args );
-					$filter              = new LP_User_Items_Filter();
+					$filter              = new UserItemsFilter();
 					$filter->only_fields = array( 'DISTINCT (item_id) AS item_id', 'ui.user_item_id' );
 					$filter->field_count = 'ui.item_id';
 					$filter->user_id     = $this->get_user_data( 'id' );
 					$filter->order_by    = 'ui.user_item_id';
 					$filter->order       = 'DESC';
+					$filter->item_type   = LP_COURSE_CPT;
 					$status              = $args['status'] ?? '';
-					if ( $status != LP_COURSE_FINISHED ) {
-						$filter->graduation = $status;
-						$filter->where[]    = $lp_user_items_db->wpdb->prepare(
-							"AND ui.status != '%s'",
-							UserItemModel::STATUS_CANCEL
-						);
-					} else {
-						$filter->status = $status;
+
+					switch ( $status ) {
+						case '':
+							$filter->where[] = $lp_user_items_db->wpdb->prepare(
+								"AND ui.status != '%s'",
+								UserItemModel::STATUS_CANCEL
+							);
+							break;
+						case UserItemModel::STATUS_FINISHED:
+							$filter->status = UserItemModel::STATUS_FINISHED;
+							break;
+						case UserItemModel::GRADUATION_IN_PROGRESS:
+						case UserItemModel::GRADUATION_PASSED:
+						case UserItemModel::GRADUATION_FAILED:
+							$filter->graduation = $status;
+							break;
 					}
+
 					$filter->page   = $args['paged'] ?? 1;
 					$filter->limit  = $args['limit'] ?? $filter->limit;
 					$total_rows     = 0;
 					$filter         = apply_filters( 'lp/api/profile/courses/purchased/filter', $filter, $args );
-					$result_courses = LP_User_Item_Course::get_user_courses( $filter, $total_rows );
+					$result_courses = UserItemsDB::getInstance()->get_user_items( $filter, $total_rows );
 
 					$course_ids = LP_Database::get_values_by_key( $result_courses, 'item_id' );
 
