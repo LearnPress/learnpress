c8f915a9fcde3ab84cbee3253489e53f5691ac74
Tungnx <tungnxt89@gmail.com>
2026-03-23 16:27:19 +0700

    #

--- inc/Models/WPTables/CoursesTable.php
+++ inc/Models/WPTables/CoursesTable.php
@@ -159,13 +159,19 @@ class CoursesTable extends WP_Posts_List_Table {
 			return;
 		}
 
+		// Count students
 		printf(
-			'<span class="lp-label-counter">%d</span>',
+			'<div class="lp-label-counter">%d</div>',
 			$courseModel->count_students()
 		);
 
+		// Button view list students
 		printf(
-			'<br><br><button type="button" class="button button-small lp-button lp-btn-view-students" data-course-id="%d" data-course-title="%s">%s</button>',
+			'<button type="button"
+				class="button button-small lp-button lp-btn-view-students"
+				data-course-id="%d"
+				data-course-title="%s"
+				style="margin-top: 10px">%s</button>',
 			$post->ID,
 			esc_attr( $post->post_title ),
 			esc_html__( 'View List', 'learnpress' )
