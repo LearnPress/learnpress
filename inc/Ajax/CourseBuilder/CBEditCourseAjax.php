c92808dbe0cd45e7dbc22df7f15aca40941f5fb5
VuxMinhThanh <vuxminhthanh@gmail.com>
2026-06-02 17:26:31 +0700

    update course category and tag handling

--- inc/Ajax/CourseBuilder/CBEditCourseAjax.php
+++ inc/Ajax/CourseBuilder/CBEditCourseAjax.php
@@ -110,6 +110,10 @@ class CBEditCourseAjax extends AbstractAjax {
 				throw new Exception( __( 'Invalid course status.', 'learnpress' ) );
 			}
 
+			// Set categories and tags
+			$courseService->update_categories( $courseModel->ID, $course_categories );
+			$courseService->update_tags( $courseModel->ID, $course_tags );
+
 			$lp_date_modified = new LP_Datetime( current_time( 'mysql' ) );
 
 			$data_save = [
@@ -182,10 +186,6 @@ class CBEditCourseAjax extends AbstractAjax {
 
 				$courseModel = CourseModel::find( $course_id, true );
 			}
-
-			// Set categories and tags
-			$courseService->update_categories( $course_id, $course_categories );
-			$courseService->update_tags( $course_id, $course_tags );
 
 			// Save or remove thumbnail
 			if ( isset( $data['course_thumbnail_id'] ) ) {
