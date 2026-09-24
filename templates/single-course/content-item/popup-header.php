2f2162399abee9530419c4f56a32ce5c204081c6
Tran Anh Tuan <tuanta@physcode.com>
2026-09-24 11:20:56 +0700

    add count quiz in single learning

--- templates/single-course/content-item/popup-header.php
+++ templates/single-course/content-item/popup-header.php
@@ -17,30 +17,41 @@ if ( ! isset( $course ) || ! isset( $user ) || ! isset( $percentage ) ||
 	! isset( $completed_items ) || ! isset( $total_items ) ) {
 	return;
 }
- 
+
 $courseModel          = CourseModel::find( $course->get_id(), true );
- 
+
 ?>
 
 <div id="popup-header">
-	
+
 	<div class="popup-header__inner">
 		<h2 class="course-title">
 			<a href="<?php echo esc_url_raw( $course->get_permalink() ); ?>"><?php echo wp_kses_post( $course->get_title() ); ?></a>
-			
-			<?php 
-				$section_count = $courseModel->get_total_sections();
-				$lesson_count  = $courseModel->count_items( LP_LESSON_CPT );
 
-				echo '<span class="curriculum-info">';
-					printf('<span class="course-count-section">%s</span>',
-						sprintf( _n( '%d Section', '%d Sections', $section_count, 'learnpress' ), $section_count )
-					);
-					printf('<span class="course-count-lesson">%s</span>',
-						sprintf( _n( '%d Lesson', '%d Lessons', $lesson_count, 'learnpress' ), $lesson_count )
-					);
-					printf( '<span class="course-duration">%s</span>', $courseModel->get_duration() );
-				echo '</span>';
+			<?php
+			$section_count = $courseModel->get_total_sections();
+			$lesson_count  = $courseModel->count_items( LP_LESSON_CPT );
+			$quiz_count    = $courseModel->count_items( LP_QUIZ_CPT );
+
+			echo '<span class="curriculum-info">';
+			printf(
+				'<span class="course-count-section">%s</span>',
+				sprintf( _n( '%d Section', '%d Sections', $section_count, 'learnpress' ), $section_count )
+			);
+			if ( $lesson_count > 0 ) {
+				printf(
+					'<span class="course-count-lesson">%s</span>',
+					sprintf( _n( '%d Lesson', '%d Lessons', $lesson_count, 'learnpress' ), $lesson_count )
+				);
+			}
+			if ( $quiz_count > 0 ) {
+				printf(
+					'<span class="course-count-quiz">%s</span>',
+					sprintf( _n( '%d Quiz', '%d Quizzes', $quiz_count, 'learnpress' ), $quiz_count )
+				);
+			}
+			printf( '<span class="course-duration">%s</span>', $courseModel->get_duration() );
+			echo '</span>';
 			?>
 		</h2>
 
